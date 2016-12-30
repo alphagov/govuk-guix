7f318f7dac0419dcdcfabf3440b0341d43a28d93
Christopher Baines <christopher.baines@digital.cabinet-office.gov.uk>
2016-12-30 11:25:43 +0000

    Move Postgresql code to its own module

--- gds/services/utils/databases.scm
+++ gds/services/utils/databases.scm
@@ -18,13 +18,7 @@
   #:use-module (gnu packages databases)
   #:use-module (gds packages govuk)
   #:use-module (gds packages mongodb)
-  #:export (<postgresql-connection-config>
-            postgresql-connection-config
-            postgresql-connection-config?
-            postgresql-connection-config-port
-            postgresql-connection-config-database
-
-            <mongodb-connection-config>
+  #:export (<mongodb-connection-config>
             mongodb-connection-config
             mongodb-connection-config?
             mongodb-connection-config-port
@@ -44,17 +38,6 @@
 
             database-connection-config?
             database-connection-config->environment-variables))
-
-
-(define-record-type* <postgresql-connection-config>
-  postgresql-connection-config make-postgresql-connection-config
-  postgresql-connection-config?
-  (host postgresql-connection-config-host
-        (default "localhost"))
-  (user postgresql-connection-config-user)
-  (port postgresql-connection-config-port
-        (default 5432))
-  (database postgresql-connection-config-database))
 
 (define-record-type* <mysql-connection-config>
   mysql-connection-config make-mysql-connection-config
@@ -153,86 +136,6 @@
     (error
      "make-database-setup: Unknown database configuration ~A"
      config))))
-
-(define (run-with-psql-port database-connection . operations)
-  (match-lambda
-    (($ <postgresql-connection-config> host user port database)
-     (with-imported-modules '((ice-9 popen))
-       #~(lambda ()
-           (let
-               ((pid (primitive-fork))
-                (postgres-user (getpwnam "postgres"))
-                (psql (string-append #$postgresql "/bin/psql")))
-             (if
-              (= 0 pid)
-              (dynamic-wind
-                (const #t)
-                (lambda ()
-                  (setgid (passwd:gid postgres-user))
-                  (setuid (passwd:uid postgres-user))
-                  (let ((p (open-pipe* OPEN_WRITE psql "-a" "-p" (number->string #$port))))
-                    (for-each
-                     (lambda (o) (o p))
-                     '#$operations)
-                    (close-pipe p))
-                  (primitive-exit 0))
-                (lambda ()
-                  (primitive-exit 1)))
-              (waitpid pid))))))))
-
-(define (postgresql-ensure-user-exists-gexp user)
-  #~(lambda (port)
-      (simple-format port "
-DO
-$body$
-BEGIN
-   IF NOT EXISTS (
-      SELECT *
-      FROM   pg_catalog.pg_user
-      WHERE  usename = '~A') THEN
-
-      CREATE ROLE \"~A\" LOGIN CREATEDB;
-   END IF;
-END
-$body$;
-" #$user #$user)))
-
-(define (postgresql-create-database-gexp database owner)
-  #~(lambda (port)
-      (simple-format port "
-CREATE DATABASE \"~A\" WITH OWNER \"~A\";" #$database #$owner)))
-
-(define (postgresql-import-gexp database-connection file)
-  (match-lambda
-    (($ <postgresql-connection-config> host user port database)
-     (with-imported-modules '((ice-9 popen))
-       #~(lambda ()
-           (let
-               ((pid (primitive-fork))
-                (postgres-user (getpwnam "postgres"))
-                (psql (string-append #$postgresql "/bin/psql")))
-             (if
-              (= 0 pid)
-              (dynamic-wind
-                (const #t)
-                (lambda ()
-                  (setgid (passwd:gid postgres-user))
-                  (setuid (passwd:uid postgres-user))
-                  (system* psql "-f" #$file "--quiet")
-                  (primitive-exit 0))
-                (lambda ()
-                  (primitive-exit 1)))
-              (waitpid pid))))))))
-
-(define (postgresql-create-user-and-database-for-database-connection
-         database-connection)
-  (run-with-psql-port
-   database-connection
-   (match database-connection
-     (($ <postgresql-connection-config> host user port database)
-      (list
-       (postgresql-ensure-user-exists-gexp user)
-       (postgresql-create-database-gexp database user))))))
 
 (define mongodb-create-user-and-database
   (match-lambda
---
+++ gds/services/utils/databases/postgresql.scm
@@ -0,0 +1,98 @@
+(define-module (gds services utils databases postgresql)
+  #:use-module (guix gexp)
+  #:use-module (guix records)
+  #:export (<postgresql-connection-config>
+            postgresql-connection-config
+            postgresql-connection-config?
+            postgresql-connection-config-port
+            postgresql-connection-config-database))
+
+(define-record-type* <postgresql-connection-config>
+  postgresql-connection-config make-postgresql-connection-config
+  postgresql-connection-config?
+  (host postgresql-connection-config-host
+        (default "localhost"))
+  (user postgresql-connection-config-user)
+  (port postgresql-connection-config-port
+        (default 5432))
+  (database postgresql-connection-config-database))
+
+(define (run-with-psql-port database-connection . operations)
+  (match-lambda
+    (($ <postgresql-connection-config> host user port database)
+     (with-imported-modules '((ice-9 popen))
+       #~(lambda ()
+           (let
+               ((pid (primitive-fork))
+                (postgres-user (getpwnam "postgres"))
+                (psql (string-append #$postgresql "/bin/psql")))
+             (if
+              (= 0 pid)
+              (dynamic-wind
+                (const #t)
+                (lambda ()
+                  (setgid (passwd:gid postgres-user))
+                  (setuid (passwd:uid postgres-user))
+                  (let ((p (open-pipe* OPEN_WRITE psql "-a" "-p" (number->string #$port))))
+                    (for-each
+                     (lambda (o) (o p))
+                     '#$operations)
+                    (close-pipe p))
+                  (primitive-exit 0))
+                (lambda ()
+                  (primitive-exit 1)))
+              (waitpid pid))))))))
+
+(define (postgresql-ensure-user-exists-gexp user)
+  #~(lambda (port)
+      (simple-format port "
+DO
+$body$
+BEGIN
+   IF NOT EXISTS (
+      SELECT *
+      FROM   pg_catalog.pg_user
+      WHERE  usename = '~A') THEN
+
+      CREATE ROLE \"~A\" LOGIN CREATEDB;
+   END IF;
+END
+$body$;
+" #$user #$user)))
+
+(define (postgresql-create-database-gexp database owner)
+  #~(lambda (port)
+      (simple-format port "
+CREATE DATABASE \"~A\" WITH OWNER \"~A\";" #$database #$owner)))
+
+(define (postgresql-import-gexp database-connection file)
+  (match-lambda
+    (($ <postgresql-connection-config> host user port database)
+     (with-imported-modules '((ice-9 popen))
+       #~(lambda ()
+           (let
+               ((pid (primitive-fork))
+                (postgres-user (getpwnam "postgres"))
+                (psql (string-append #$postgresql "/bin/psql")))
+             (if
+              (= 0 pid)
+              (dynamic-wind
+                (const #t)
+                (lambda ()
+                  (setgid (passwd:gid postgres-user))
+                  (setuid (passwd:uid postgres-user))
+                  (system* psql "-f" #$file "--quiet")
+                  (primitive-exit 0))
+                (lambda ()
+                  (primitive-exit 1)))
+              (waitpid pid))))))))
+
+(define (postgresql-create-user-and-database-for-database-connection
+         database-connection)
+  (run-with-psql-port
+   database-connection
+   (match database-connection
+     (($ <postgresql-connection-config> host user port database)
+      (list
+       (postgresql-ensure-user-exists-gexp user)
+       (postgresql-create-database-gexp database user))))))
