b0dde7783ce6ab64aa208988d7c892eac58eaad2
Christopher Baines <christopher.baines@digital.cabinet-office.gov.uk>
2016-12-31 12:50:22 +0000

    Move mysql functionality to a seperate file

---
+++ gds/services/utils/databases/mysql.scm
@@ -0,0 +1,93 @@
+(define-module (gds services utils databases mysql)
+  #:use-module (ice-9 match)
+  #:use-module (guix gexp)
+  #:use-module (guix records)
+  #:use-module (gnu packages databases)
+  #:export (<mysql-connection-config>
+            mysql-connection-config
+            mysql-connection-config?
+            mysql-connection-config-port
+            mysql-connection-config-database
+
+            run-with-mysql-port
+            mysql-ensure-user-exists-gexp
+            mysql-create-database-gexp
+            mysql-create-user-and-database-for-database-connection))
+
+(define-record-type* <mysql-connection-config>
+  mysql-connection-config make-mysql-connection-config
+  mysql-connection-config?
+  (host mysql-connection-config-host
+        (default "localhost"))
+  (user mysql-connection-config-user)
+  (port mysql-connection-config-port
+        (default 3306))
+  (database mysql-connection-config-database)
+  (password mysql-connection-config-password))
+
+(define (run-with-mysql-port database-connection operations)
+  (match database-connection
+    (($ <mysql-connection-config> host user port database)
+     (with-imported-modules '((ice-9 popen))
+       #~(lambda ()
+           (let
+               ((pid (primitive-fork))
+                (root (getpwnam "root"))
+                (command `(#$mysql
+                           "-h" #$host
+                           "-u" "root"
+                           "--password=''"
+                           "-P" ,(number->string #$port))))
+             (if
+              (= 0 pid)
+              (dynamic-wind
+                (const #t)
+                (lambda ()
+                  (setgid (passwd:gid root))
+                  (setuid (passwd:uid root))
+                  (let ((p (open-pipe* OPEN_WRITE
+                                       (string-join
+                                        command
+                                        " ")
+                                       (number->string
+                                        #$port))))
+                    (for-each
+                     (lambda (o) (o p))
+                     '#$operations)
+                    (close-pipe p))
+                  (primitive-exit 0))
+                (lambda ()
+                  (primitive-exit 1)))
+              (waitpid pid))))))))
+
+(define (mysql-ensure-user-exists-gexp user password)
+  #~(lambda (port)
+      (define (log-and-write p str . args)
+        (display (apply simple-format #f str args))(display "\n")
+        (apply simple-format p str args))
+
+      (log-and-write port "
+CREATE USER IF NOT EXISTS '~A'@'localhost' IDENTIFIED BY '~A';\n
+" #$user #$password)))
+
+(define (mysql-create-database-gexp database user)
+  #~(lambda (port)
+      (define (log-and-write p str . args)
+        (display (apply simple-format #f str args))(display "\n")
+        (apply simple-format p str args))
+
+      (log-and-write port "
+CREATE DATABASE ~A;\n" #$database)
+      (log-and-write port "
+GRANT ALL ON ~A.* TO '~A'@'localhost';\n" #$database #$user)
+      (log-and-write port "EXIT\n")))
+
+(define (mysql-create-user-and-database-for-database-connection
+         database-connection)
+  (run-with-mysql-port
+   database-connection
+   (match database-connection
+     (($ <mysql-connection-config> host user port database password)
+      (list
+       (mysql-ensure-user-exists-gexp user password)
+       (mysql-create-database-gexp database user))))))
