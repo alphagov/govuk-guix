a6c01d0339fdaa8711bb6791044ca6404a6c133c
Christopher Baines <christopher.baines@digital.cabinet-office.gov.uk>
2017-01-21 22:53:27 +0000

    Add isolated version of publishing-e2e-tests-os
    This replaces the dummy-loopback-service with a
    static-networking-service, as this is necessary when not sharing the
    host network.

---
+++ gds/systems/govuk/publishing-e2e-tests-isolated.scm
@@ -0,0 +1,23 @@
+(define-module (gds systems govuk publishing-e2e-tests-isolated)
+  #:use-module (gnu system)
+  #:use-module (gnu services)
+  #:use-module (gnu services networking)
+  #:use-module (gds systems govuk publishing-e2e-tests))
+
+(define-public publishing-e2e-tests-isolated-os
+  (operating-system
+    (inherit publishing-e2e-tests-os)
+    (services
+     (cons
+      (static-networking-service "lo" "127.0.0.1"
+                                 #:provision '(loopback))
+      (filter
+       (lambda (s)
+         (not (eq?
+               'dummy-loopback-service
+               (service-type-name
+                (service-kind s)))))
+       (operating-system-user-services
+        publishing-e2e-tests-os))))))
+
+publishing-e2e-tests-isolated-os
