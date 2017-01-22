4c9caad38dc21074e1a19b04f2c19b9f4fa0b636
Christopher Baines <christopher.baines@digital.cabinet-office.gov.uk>
2017-01-22 17:17:28 +0000

    Copy the make-git-predicate from Guix
    This helps when using a local copy of Guix, as otherwise, the build
    process for the guix package can trip up over files that are a result
    of the build process when performed directly on the source.

--- gds/packages/guix.scm
+++ gds/packages/guix.scm
@@ -1,17 +1,64 @@
 (define-module (gds packages guix)
+  #:use-module (srfi srfi-1)
+  #:use-module (srfi srfi-26)
+  #:use-module (ice-9 popen)
+  #:use-module (ice-9 match)
+  #:use-module (ice-9 rdelim)
+  #:use-module (guix gexp)
   #:use-module (guix packages)
   #:use-module (guix utils)
+  #:use-module (guix build utils)
   #:use-module (guix git-download)
   #:use-module ((gnu packages package-management) #:prefix gnu:))
+
+(define (make-git-predicate directory)
+  "Return a predicate that returns true if a file is part of the Git checkout
+living at DIRECTORY.  Upon Git failure, return #f instead of a predicate."
+  (define (parent-directory? thing directory)
+    ;; Return #t if DIRECTORY is the parent of THING.
+    (or (string-suffix? thing directory)
+        (and (string-index thing #\/)
+             (parent-directory? (dirname thing) directory))))
+
+  (let* ((pipe        (with-directory-excursion directory
+                        (open-pipe* OPEN_READ "git" "ls-files")))
+         (files       (let loop ((lines '()))
+                        (match (read-line pipe)
+                          ((? eof-object?)
+                           (reverse lines))
+                          (line
+                           (loop (cons line lines))))))
+         (status      (close-pipe pipe)))
+    (and (zero? status)
+         (lambda (file stat)
+           ;(simple-format #t "~A\n" file)
+           (let ((foo
+                  (match (stat:type stat)
+             ('directory
+              ;; 'git ls-files' does not list directories, only regular files,
+              ;; so we need this special trick.
+              (any (cut parent-directory? <> file) files))
+             ((or 'regular 'symlink)
+              (any (cut string-suffix? <> file) files))
+             (_
+              #f))))
+             ;(simple-format #t "~A\n" foo)
+             foo)
+                  ))))
 
 (define-public guix
   ;; Note that when changing the treeish, you will need to change the
   ;; sha256 hash such that Guix thinks that it has not built this
-  (let ((treeish "spike-end-to-end-test-system"))
+  (let ((treeish "spike-end-to-end-test-system")
+        (select? (delay (make-git-predicate
+                         (getenv "GDS_GNU_GUIX_PATH"))))
+        (local-source (string? (getenv "GDS_GNU_GUIX_PATH"))))
     (package
       (inherit gnu:guix)
       (name "guix-gds")
-      (version (string-append "0.11.0"))
+      (version (if local-source
+                   "local"
+                   "0.11.0"))
       (arguments
        (substitute-keyword-arguments (package-arguments gnu:guix)
          ((#:tests? tests)
@@ -20,7 +67,10 @@
           `(modify-phases ,phases
              (delete 'check)))))
       (source
-       (or (getenv "GDS_GNU_GUIX_PATH")
+       (if local-source
+           (local-file (getenv "GDS_GNU_GUIX_PATH") "guix-gds"
+                       #:recursive? #t
+                       #:select? (force select?))
            (origin
              (method git-fetch)
              (uri (git-reference
