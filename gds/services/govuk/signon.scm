1ae3703e86d548ad02848ada30c440b6fa862c12
Christopher Baines <christopher.baines@digital.cabinet-office.gov.uk>
2017-02-21 06:39:13 +0000

    Remove gds-sso-config
    The service-startup-config can be used to set this without the need
    for more code.

--- gds/services/govuk/signon.scm
+++ gds/services/govuk/signon.scm
@@ -8,14 +8,7 @@
             signon-application-redirect-uri
             signon-application-home-uri
             signon-application-uid
-            signon-application-secret
-
-            <gds-sso-config>
-            gds-sso-config
-            gds-sso-config?
-            gds-sso-config-strategy
-
-            gds-sso-config->environment-variables))
+            signon-application-secret))
 
 (define-record-type* <signon-application>
   signon-application make-signon-application
@@ -28,13 +21,3 @@
        (default #f))
   (secret signon-application-name
           (default #f)))
-
-(define-record-type* <gds-sso-config>
-  gds-sso-config make-gds-sso-config
-  gds-sso-config?
-  (strategy gds-sso-config-strategy
-            (default "real")))
-
-(define (gds-sso-config->environment-variables gds-sso-config)
-  (list
-   (cons "GDS_SSO_STRATEGY" (gds-sso-config-strategy gds-sso-config))))
