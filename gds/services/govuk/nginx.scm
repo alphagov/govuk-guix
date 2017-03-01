087b667a92a243745fa175650a66544a7b7d3b20
Christopher Baines <christopher.baines@digital.cabinet-office.gov.uk>
2017-03-01 19:28:58 +0000

    Proxy assets for frontend

--- gds/services/govuk/nginx.scm
+++ gds/services/govuk/nginx.scm
@@ -69,6 +69,9 @@
        (locations
         (list
          (nginx-location-configuration
+          (uri "/frontend")
+          (body '("proxy_pass http://frontend-proxy;")))
+         (nginx-location-configuration
           (uri "/info-frontend")
           (body '("proxy_pass http://info-frontend-proxy;")))
          (nginx-location-configuration
