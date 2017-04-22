69dc2b43c5747032b3e56c892d524f904643a5f5
Christopher Baines <christopher.baines@digital.cabinet-office.gov.uk>
2017-04-22 20:51:07 +0100

    Add a rails build system
    This includes customisations on top of the GNU build system that make
    it easier to package rails applications.

---
+++ gds/build-system/rails.scm
@@ -0,0 +1,108 @@
+(define-module (gds build-system rails)
+  #:use-module (guix store)
+  #:use-module (guix utils)
+  #:use-module (guix packages)
+  #:use-module (guix derivations)
+  #:use-module (guix search-paths)
+  #:use-module (guix build-system)
+  #:use-module (guix build-system gnu)
+  #:use-module (ice-9 match)
+  #:use-module (srfi srfi-26)
+  #:use-module (gnu packages node)
+  #:export (%rails-build-system-modules
+            rails-build
+            rails-build-system))
+
+(define %rails-build-system-modules
+  ;; Build side modules imported by default
+  `((gds build rails-build-system)
+    (guix build syscalls)
+    ,@%gnu-build-system-modules))
+
+(define (default-ruby)
+  (let ((ruby-mod (resolve-interface '(gnu packages ruby))))
+    (module-ref ruby-mod 'ruby)))
+
+(define* (lower name
+                #:key source inputs native-inputs outputs system target
+                (ruby (default-ruby))
+                #:allow-other-keys
+                #:rest arguments)
+  "Return a bag for NAME."
+  (define private-keywords
+    '(#:source #:target #:inputs #:native-inputs))
+
+  (and (not target)                               ;XXX: no cross-compilation
+       (bag
+         (name name)
+         (system system)
+         (host-inputs `(,@(if source
+                              `(("source" ,source))
+                              '())
+                        ,@inputs
+
+                        ;; Keep the standard inputs of 'gnu-build-system'.
+                        ,@(standard-packages)))
+         (build-inputs `(("ruby" ,ruby)
+                         ("node" ,node) ;; Rails seems to have a
+                                        ;; transtive dependency on
+                                        ;; node, or some Javascript
+                                        ;; interpreter
+                         ,@native-inputs))
+         (outputs outputs)
+         (build rails-build)
+         (arguments (strip-keyword-arguments private-keywords arguments)))))
+
+(define* (rails-build store name inputs
+                      #:key
+                      (phases '(@ (gds build rails-build-system)
+                                  %standard-phases))
+                      (imported-modules %rails-build-system-modules)
+                      (system (%current-system))
+                      (modules '((gds build rails-build-system)
+                                 (guix build utils)))
+                      (search-paths '())
+                      (outputs '("out"))
+                      (precompile-rails-assets? #t)
+                      (guile #f))
+  "Build SOURCE with INPUTS."
+  (define builder
+    `(begin
+       (use-modules ,@modules)
+       (rails-build #:name ,name
+                    #:source ,(match (assoc-ref inputs "source")
+                                (((? derivation? source))
+                                 (derivation->output-path source))
+                                ((source)
+                                 source)
+                                (source
+                                 source))
+                    #:system ,system
+                    #:search-paths ',(map search-path-specification->sexp
+                                          search-paths)
+                    #:phases ,phases
+                    #:system ,system
+                    #:outputs %outputs
+                    #:inputs %build-inputs)))
+
+  (define guile-for-build
+    (match guile
+      ((? package?)
+       (package-derivation store guile system #:graft? #f))
+      (#f                               ; the default
+       (let* ((distro (resolve-interface '(gnu packages commencement)))
+              (guile  (module-ref distro 'guile-final)))
+         (package-derivation store guile system #:graft? #f)))))
+
+  (build-expression->derivation store name builder
+                                #:inputs inputs
+                                #:system system
+                                #:modules imported-modules
+                                #:outputs outputs
+                                #:guile-for-build guile-for-build))
+
+(define rails-build-system
+  (build-system
+    (name 'rails)
+    (description "Build system for Rails applications")
+    (lower lower)))
---
+++ gds/build/rails-build-system.scm
@@ -0,0 +1,120 @@
+(define-module (gds build rails-build-system)
+  #:use-module ((guix build gnu-build-system) #:prefix gnu:)
+  #:use-module (guix build syscalls)
+  #:use-module (guix build utils)
+  #:use-module (sxml simple)
+  #:use-module (ice-9 match)
+  #:use-module (ice-9 ftw)
+  #:use-module (srfi srfi-1)
+  #:use-module (srfi srfi-26)
+  #:export (%standard-phases
+            rails-build))
+
+;; Commentary:
+;;
+;; Builder-side code of the standard build procedure for applications
+;; using Rails.
+;;
+;; Code:
+
+(define* (replace-relative-spring-path #:key outputs
+                                       #:allow-other-keys)
+  (let* ((out (assoc-ref outputs "out"))
+         (files
+          (find-files
+           (string-append out "/bin")
+           (lambda (name stat)
+             (or
+              (access? name X_OK)
+              (begin
+                (simple-format
+                 #t
+                 "Skipping wrapping ~A as its not executable\n" name)
+                #f))))))
+    (substitute* files
+      (("File\\.expand_path\\([\"']\\.\\./spring[\"'], __FILE__\\)")
+       "File.expand_path('../.spring-real', __FILE__)"))))
+
+(define* (wrap-with-relative-path #:key outputs
+                                  #:allow-other-keys)
+  (let* ((out (assoc-ref outputs "out"))
+         (files
+          (find-files
+           (string-append out "/bin")
+           (lambda (name stat)
+             (or
+              (access? name X_OK)
+              (begin
+                (simple-format
+                 #t
+                 "Skipping wrapping ~A as its not executable\n" name)
+                #f))))))
+    (substitute* files
+      (((string-append out "/bin"))
+       "${BASH_SOURCE%/*}"))))
+
+(define* (create-tmp-directory #:key outputs
+                              #:allow-other-keys)
+  (mkdir-p (string-append
+            (assoc-ref outputs "out")
+            "/tmp")))
+
+(define* (create-log-directory #:key outputs #:allow-other-keys)
+  (mkdir-p (string-append
+            (assoc-ref outputs "out")
+            "/log")))
+
+(define* (precompile-rails-assets #:key inputs #:allow-other-keys)
+  (zero?
+   (system* "bundle" "exec" "rake" "assets:precompile")))
+
+(define* (install #:key inputs outputs #:allow-other-keys)
+  (let* ((out (assoc-ref outputs "out")))
+    (copy-recursively
+     "."
+     out
+     #:log (%make-void-port "w"))))
+
+(define* (patch-bin-files #:key inputs outputs #:allow-other-keys)
+  (let* ((out (assoc-ref outputs "out")))
+    (substitute*
+        (find-files
+         (string-append out "/bin")
+         (lambda (name stat)
+           (or
+            (access? name X_OK)
+            (begin
+              (simple-format #t "Skipping patching ~A as its not executable\n" name)
+              #f))))
+      (("/usr/bin/env") (which "env")))))
+
+(define %standard-phases
+  (modify-phases gnu:%standard-phases
+    (replace 'configure (lambda args #t))
+    (replace 'build (lambda args #t))
+    (replace 'check (lambda args #t))
+    (replace 'install install)
+    (add-after 'install 'create-tmp-directory
+               create-tmp-directory)
+    (add-after 'create-tmp-directory 'create-log-directory
+               create-log-directory)
+    (add-after 'create-log-directory 'patch-bin-files
+               patch-bin-files)))
+
+(define* (rails-build #:key
+                      inputs
+                      (phases %standard-phases)
+                      (precompile-rails-assets? #t)
+                      #:allow-other-keys
+                      #:rest args)
+  "Build the given Rails application, applying all of PHASES in order."
+  (apply gnu:gnu-build
+         #:inputs inputs
+         #:phases (filter
+                   (match-lambda
+                     ((key . value)
+                      (cond ((eq? key 'precompile-rails-assets)
+                             precompile-rails-assets?)
+                            (else #f))))
+                   phases)
+         args))
