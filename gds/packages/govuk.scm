3a204be231dd198b90f443f78cf1287be087ca08
Package Updater <govuk-guix-package-updater@digital.cabinet-office.gov.uk>
2017-04-27 17:02:09 +0000

    Update content-api from release_402 to release_403

--- gds/packages/govuk.scm
+++ gds/packages/govuk.scm
@@ -223,12 +223,12 @@ proxies requests to some upstream")
     (hash (base32 "1iaf3vyiff00sybr0ysqx20j68ml0kzzkb9sv05w4xy612shbyva")))
    (package
      (name "content-api")
-     (version "release_402")
+     (version "release_403")
      (source
       (github-archive
        #:repository "govuk_content_api"
        #:commit-ish version
-       #:hash (base32 "1hhabbbvma9pk13hf38c5jx9zm3zdi2683sifghg8hg9wx79f946")))
+       #:hash (base32 "1rj8j9ca8x4k4dlv3f2dx1j5klsf0ldhbp5pz2s2xys2n7h9k6m1")))
      (build-system rails-build-system)
      (arguments
       `(#:precompile-rails-assets? #f
