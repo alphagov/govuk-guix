97e735ac12944360fa43d6f4f1e13caa1260176c
Christopher Baines <christopher.baines@digital.cabinet-office.gov.uk>
2017-04-27 07:18:08 +0100

    Add a service to set file ownership upon system activation

--- gds/services/base.scm
+++ gds/services/base.scm
@@ -1,4 +1,5 @@
 (define-module (gds services base)
+  #:use-module (srfi srfi-1)
   #:use-module (gnu services)
   #:use-module (gnu services shepherd)
   #:use-module (gnu system shadow)
@@ -11,7 +12,8 @@
   #:use-module (gnu packages base)
   #:use-module (gnu packages databases)
   #:use-module (gds packages govuk)
-  #:export (pretend-loopback-service))
+  #:export (pretend-loopback-service
+            set-file-ownership-service-type))
 
 (define pretend-loopback-service
   (service
@@ -24,3 +26,31 @@
       (start #~(const #t))
       (stop #~(const #t)))))
    '()))
+
+(define set-file-ownership-service-type
+  (service-type
+   (name 'set-file-ownership)
+   (extensions
+    (list (service-extension
+           activation-service-type
+           (lambda (paths-and-owners)
+             #~(begin
+                 (use-modules (ice-9 match))
+                 (for-each
+                  (match-lambda
+                    ((path user group options ...)
+                     (if (file-exists? path)
+                         (let
+                             ((command
+                               `(,#$(file-append coreutils "/bin/chown")
+                                 ,(simple-format #f "~A:~A" user group)
+                                 ,@(if (memq #:recursive options) '("-R") '())
+                                 ,path)))
+                           (simple-format #t "Changing owner of ~A to ~A:~A\n"
+                                          path user group)
+                           (simple-format #t "Running command: ~A\n" (string-join command))
+                           (apply system* command))
+                         (simple-format #t "Skipping ~A as it does not exist\n" path))))
+                  '#$paths-and-owners))))))
+   (compose concatenate)
+   (extend append)))
