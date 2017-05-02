264c0e40abf768410a73c75fab7c938750f6554c
Package Updater <govuk-guix-package-updater@digital.cabinet-office.gov.uk>
2017-05-02 10:01:58 +0000

    Update static from release_2552 to release_2553

--- gds/packages/govuk.scm
+++ gds/packages/govuk.scm
@@ -1228,12 +1228,12 @@ content, as well as broadcasting changes to a message queue.")
     (hash (base32 "0gxry4hdp5scn5majhfmq8zn7sz6bxpfqmxq6r59024a9biybawp")))
    (package
      (name "static")
-     (version "release_2552")
+     (version "release_2553")
      (source
       (github-archive
        #:repository name
        #:commit-ish version
-       #:hash (base32 "1vq2jb5y170mm9nhry252dfsdqfxywf9jr7xdvr9qmlah6ws3f9z")))
+       #:hash (base32 "0gba4d2qnhp46jsk5qk9cbswifgpjn385l8kcsl2xcsbn9i0h2ch")))
      (build-system rails-build-system)
      (arguments `(#:precompile-rails-assets? #f))
      (synopsis "")
