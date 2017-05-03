25b3c97e670e37cabb9e885039e7118197698ce0
Package Updater <govuk-guix-package-updater@digital.cabinet-office.gov.uk>
2017-05-03 14:02:03 +0000

    Update search-admin from release_93 to release_94

--- gds/packages/govuk.scm
+++ gds/packages/govuk.scm
@@ -1055,12 +1055,12 @@ content, as well as broadcasting changes to a message queue.")
     (hash (base32 "06wy96q5przv7pnc1wxxkl606jyayhyn3si3fi93v9c3rwaiv0sk")))
    (package
      (name "search-admin")
-     (version "release_93")
+     (version "release_94")
      (source
       (github-archive
        #:repository name
        #:commit-ish version
-       #:hash (base32 "0b0w0p9rimi9g07cidpk13yb71vr9qka37x8agfdxz3xz5rzwlbb")))
+       #:hash (base32 "0d96isafqvs8ysav5qgl3l6zj5pah671y1j0fj5v54zkl319crla")))
      (build-system rails-build-system)
      (synopsis "")
      (description "")
