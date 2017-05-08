aded741b1216e7393f4f01c2cbb80037a2afdd1f
Package Updater <govuk-guix-package-updater@digital.cabinet-office.gov.uk>
2017-05-08 11:41:19 +0000

    Update manuals-publisher from release_900 to release_902

--- gds/packages/govuk.scm
+++ gds/packages/govuk.scm
@@ -701,12 +701,12 @@ service setup.")
     (hash (base32 "04k90s17afg0g3z0331rzd88mlbhk4nlsp3z799nm1w8klvpjrn2")))
    (package
      (name "manuals-publisher")
-     (version "release_900")
+     (version "release_902")
      (source
       (github-archive
        #:repository name
        #:commit-ish version
-       #:hash (base32 "1ncrjs874zsgnij99071xkdfpvvgx10arzi7cnzi1yz81i6pjf6q")))
+       #:hash (base32 "0cr1b2816kv36nynckwdxsjadlnc1yry3myyhp3b593r08qvp8ab")))
      (build-system rails-build-system)
      (arguments
       `(#:precompile-rails-assets? #f ;; Asset precompilation fails
