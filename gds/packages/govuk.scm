81787b98825429fca4ccd58765e41b4981984bc5
Package Updater <govuk-guix-package-updater@digital.cabinet-office.gov.uk>
2017-05-11 10:40:05 +0000

    Update content-tagger from release_408 to release_409

--- gds/packages/govuk.scm
+++ gds/packages/govuk.scm
@@ -310,12 +310,12 @@ proxies requests to some upstream")
     (hash (base32 "0syxhixz96khcj8rmjy0s9yy14xal8d75ja6ar69mh9ylr9argg6")))
    (package
      (name "content-tagger")
-     (version "release_408")
+     (version "release_409")
      (source
       (github-archive
        #:repository name
        #:commit-ish version
-       #:hash (base32 "15p9ndyhrcaga25dch87ad6yf46mqappnzp3vdilsgz7pn3z3yyr")))
+       #:hash (base32 "16ip76fdgyr5hggzd9a4r54ihn4pj77zj1m8yihdg9g7vljk5cfw")))
      (build-system rails-build-system)
      (synopsis "")
      (description "")
