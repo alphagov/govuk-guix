2276cbdec50cdc1432369a69daf700cffc58792e
Package Updater <govuk-guix-package-updater@digital.cabinet-office.gov.uk>
2017-06-19 14:10:05 +0000

    Update specialist-publisher from release_815 to release_816

--- gds/packages/govuk.scm
+++ gds/packages/govuk.scm
@@ -1159,12 +1159,12 @@ content, as well as broadcasting changes to a message queue.")
     (without '("development" "test")))
    (package
      (name "specialist-publisher")
-     (version "release_815")
+     (version "release_816")
      (source
       (github-archive
        #:repository name
        #:commit-ish version
-       #:hash (base32 "1h8z4m9pmqnm9k6bc42kb01msmvv8rrivispgwkqx8fiys9fjrzp")))
+       #:hash (base32 "0cq5yf3vbw3pqndmxxd8hgndj91gd81z5ql5yj89ymhxhvma7iz1")))
      (build-system rails-build-system)
      (arguments
       `(#:phases
