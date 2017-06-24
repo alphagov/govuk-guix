b873349270bbc9dff432ee862402f599855340ec
Christopher Baines <christopher.baines@digital.cabinet-office.gov.uk>
2017-06-24 19:10:00 +0100

    Change postgresql-create-database-gexp
    Have it check if the database exists, and if it does not, then create
    it. Also rename the function to make the purpose clearer.

--- gds/services/utils/databases/postgresql.scm
+++ gds/services/utils/databases/postgresql.scm
@@ -64,10 +64,20 @@ END
 $body$;
 " #$user #$user)))
 
-(define (postgresql-create-database-gexp database owner)
+(define (postgresql-ensure-database-exists-gexp database owner)
   #~(lambda (port)
       (simple-format port "
-CREATE DATABASE \"~A\" WITH OWNER \"~A\";" #$database #$owner)))
+DO
+$do$
+BEGIN
+   IF EXISTS (SELECT 1 FROM pg_database WHERE datname = '~A') THEN
+      RAISE NOTICE 'postgresql-ensure-database-exists: ~A already exists';
+   ELSE
+      PERFORM dblink_exec('dbname=' || current_database(), 'CREATE DATABASE ~A OWNER ~A');
+   END IF;
+END
+$do$;
+" #$database #$database #$database #$owner)))
 
 (define (postgresql-import-gexp database-connection file)
   (match database-connection
@@ -103,4 +113,4 @@ CREATE DATABASE \"~A\" WITH OWNER \"~A\";" #$database #$owner)))
      (($ <postgresql-connection-config> host user port database)
       (list
        (postgresql-ensure-user-exists-gexp user)
-       (postgresql-create-database-gexp database user))))))
+       (postgresql-ensure-database-exists-gexp database user))))))
