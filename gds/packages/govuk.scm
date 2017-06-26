1bc7177b309e990a7239e51fbbe0868eda08a4f2
Package Updater <govuk-guix-package-updater@digital.cabinet-office.gov.uk>
2017-06-26 11:09:43 +0000

    Update rummager from release_1370 to release_1371

--- gds/packages/govuk.scm
+++ gds/packages/govuk.scm
@@ -1028,12 +1028,12 @@ content, as well as broadcasting changes to a message queue.")
     (hash (base32 "1plpgni5nc8lnmjvc4rhyrvhg0h3hphnq6ld58qg55qyamimmdwk")))
    (package
      (name "rummager")
-     (version "release_1370")
+     (version "release_1371")
      (source
       (github-archive
        #:repository name
        #:commit-ish version
-       #:hash (base32 "1rpli3iq9cm6y3cbdi236wvil9mviadskiz5hgllzxjpy9x3k303")))
+       #:hash (base32 "0rv7s1bs3wwm70a6ln6nr4xrs0dibcap6ii96cza2yf6qzk6dck9")))
      (build-system rails-build-system)
      (arguments
       `(#:precompile-rails-assets? #f
