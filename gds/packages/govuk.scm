54dd69af2bf84743f9e85823939b8d7e1d68a203
Package Updater <govuk-guix-package-updater@digital.cabinet-office.gov.uk>
2017-07-20 11:10:31 +0000

    Update content-performance-manager from release_156 to release_157

--- gds/packages/govuk.scm
+++ gds/packages/govuk.scm
@@ -255,12 +255,12 @@ proxies requests to some upstream")
     (hash (base32 "1rqbksnjg0s1731g002sjj8psqp0k3dqkgir8g11qqjimjw8c62x")))
    (package
      (name "content-performance-manager")
-     (version "release_156")
+     (version "release_157")
      (source
       (github-archive
        #:repository name
        #:commit-ish version
-       #:hash (base32 "0bgp4yn29wxlgywr09gmxgv90y7hzwd7447cn27hz098wzkflx27")))
+       #:hash (base32 "08kcdz5lwrc25aysvhhxahpjp6pa2p4lmc5y44jpcc949wqza4m2")))
      (build-system rails-build-system)
      (arguments
       `(#:phases
