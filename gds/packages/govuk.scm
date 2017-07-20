bedf57c3f5bdd204e3df1da45c4dc86755e3a8bf
Package Updater <govuk-guix-package-updater@digital.cabinet-office.gov.uk>
2017-07-20 12:39:40 +0000

    Update hmrc-manuals-api from release_183 to release_184

--- gds/packages/govuk.scm
+++ gds/packages/govuk.scm
@@ -572,12 +572,12 @@ service setup.")
     (hash (base32 "0wxqylhp5rfmkhd9aylg6wb1m2iqjfrap3f8p6nmykbqwld5sy4w")))
    (package
      (name "hmrc-manuals-api")
-     (version "release_183")
+     (version "release_184")
      (source
       (github-archive
        #:repository name
        #:commit-ish version
-       #:hash (base32 "1093gigxg02r7fxjmjq36zayl7n7460rs7rvd8zv9v9pf4fddfvw")))
+       #:hash (base32 "1h5aw51yxr3shhvbxcgs3m9sx4hfwvjwar5iyrva3b53gqgvivay")))
      (build-system rails-build-system)
      (arguments `(#:precompile-rails-assets? #f))
      (synopsis "")
