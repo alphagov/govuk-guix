c3a04db4a32f885a3f33db7b1961dabb2c2d2b31
Package Updater <govuk-guix-package-updater@digital.cabinet-office.gov.uk>
2017-08-01 15:09:49 +0000

    Update publishing-api from release_954 to release_955

--- gds/packages/govuk.scm
+++ gds/packages/govuk.scm
@@ -870,12 +870,12 @@ service setup.")
     (hash (base32 "19hh5488y1fsn1ckdr2cvg3cn997lf8swv84jjshv2bw6jg5q8l1")))
    (package
      (name "publishing-api")
-     (version "release_954")
+     (version "release_955")
      (source
       (github-archive
        #:repository "publishing-api"
        #:commit-ish version
-       #:hash (base32 "1fvr7fhm4hxljwkmzb5ajs7il34vnlclr80vanwh312mncgzc3b4")))
+       #:hash (base32 "1qk9zyi3vndnd20b6qgd1cfsh7x1wy7d11mf704lm85780b7n0zy")))
      (build-system rails-build-system)
      (arguments `(#:precompile-rails-assets? #f
                   #:ruby ,ruby-2.3))
