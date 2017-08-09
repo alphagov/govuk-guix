350462044729b676bdab56d6784c2a2fbb49e675
Package Updater <govuk-guix-package-updater@digital.cabinet-office.gov.uk>
2017-08-09 10:09:35 +0000

    Update info-frontend from release_81 to release_82

--- gds/packages/govuk.scm
+++ gds/packages/govuk.scm
@@ -612,12 +612,12 @@ service setup.")
     (hash (base32 "1a22s5q11gqw9l79p89dln5pz72rl8kypa98lrh8941b37gq3gni")))
    (package
      (name "info-frontend")
-     (version "release_81")
+     (version "release_82")
      (source
       (github-archive
        #:repository name
        #:commit-ish version
-       #:hash (base32 "08796lv1ncm2vfzsjkwwi7mp7h81v55y8qgaz5kr01bjczcy8jah")))
+       #:hash (base32 "0vl62kl2nb1g7vnglixqrh1vf600g29vfgbc6j38fcf90jp2sk9i")))
      (build-system rails-build-system)
      (arguments `(#:precompile-rails-assets? #f))
      (synopsis "")
