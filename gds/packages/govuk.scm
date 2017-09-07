799aec91bda388f9c8e56f5580e59e02b50dbf6e
Package Updater <govuk-guix-package-updater@digital.cabinet-office.gov.uk>
2017-09-07 09:10:44 +0000

    Update collections from release_361 to release_362

--- gds/packages/govuk.scm
+++ gds/packages/govuk.scm
@@ -191,12 +191,12 @@ proxies requests to some upstream")
     (hash (base32 "1nvaz45kmsmrc0q8kd1kvdvcw3jims81wqwmanb0d5bjkwzhr5r9")))
    (package
      (name "collections")
-     (version "release_361")
+     (version "release_362")
      (source
       (github-archive
        #:repository name
        #:commit-ish version
-       #:hash (base32 "0r8a4spcvplq631kyhc1kzy0bg02da6rnj60ihjfsd0axk06j5ys")))
+       #:hash (base32 "0pjnmw5x9vfki03z7b3a29kvi4vsbxgmj36rjl5rmz5mhpnj0v4v")))
      (build-system rails-build-system)
      (synopsis "Collections serves the new GOV.UK navigation and other pages")
      (description "")
