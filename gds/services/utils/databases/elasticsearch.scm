ad30b16877fb57a3e910b33a95d065076a416bea
Christopher Baines <christopher.baines@digital.cabinet-office.gov.uk>
2017-09-24 10:12:52 +0100

    Add a procedure elasticsearch-restore-gexp

--- gds/services/utils/databases/elasticsearch.scm
+++ gds/services/utils/databases/elasticsearch.scm
@@ -4,12 +4,13 @@
   #:use-module (guix records)
   #:use-module (gnu packages databases)
   #:use-module (gnu packages compression)
-  #:use-module (gnu packages pv)
   #:export (<elasticsearch-connection-config>
             elasticsearch-connection-config
             elasticsearch-connection-config?
             elasticsearch-connection-config-port
-            elasticsearch-connection-config-host))
+            elasticsearch-connection-config-host
+
+            elasticsearch-restore-gexp))
 
 (define-record-type* <elasticsearch-connection-config>
   elasticsearch-connection-config make-elasticsearch-connection-config
@@ -18,3 +19,22 @@
         (default "localhost"))
   (port elasticsearch-connection-config-port
         (default 5432)))
+
+(define* (elasticsearch-restore-gexp database-connection index-name file
+                                     #:key alias overrides batch-size)
+  (match database-connection
+    (($ <elasticsearch-connection-config> host port)
+     #~(lambda ()
+         (let ((command
+                (list
+                 (string-append #$es-dump-restore "/bin/es_dump_restore")
+                 #$(if alias "restore_alias" "restore")
+                 #$(simple-format #f "http://~A:~A" host (number->string port))
+                 #$@(if alias (list alias) '())
+                 #$index-name
+                 #$file
+                 #$@(if overrides (list overrides) '())
+                 #$@(if batch-size (list (number->string batch-size)) '()))))
+           (simple-format #t "Running command: ~A\n" (string-join command " "))
+           (zero?
+            (apply system* command)))))))
