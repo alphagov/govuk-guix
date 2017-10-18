cb0e1cee2da64d5645c4bde83c3656cd7fd0b408
Package Updater <govuk-guix-package-updater@digital.cabinet-office.gov.uk>
2017-10-18 16:45:43 +0000

    Update authenticating-proxy from release_38 to release_39

--- gds/packages/govuk.scm
+++ gds/packages/govuk.scm
@@ -109,12 +109,12 @@
     (hash (base32 "00qm6xwq7qsmivy7p79wcgz5lb6ndfzfb75lcf9zn5rd888wi6x2")))
    (package
      (name "authenticating-proxy")
-     (version "release_38")
+     (version "release_39")
      (source
       (github-archive
        #:repository name
        #:commit-ish version
-       #:hash (base32 "1cbri1zh9ydpdpzsqyqzbp9c0822r9nw8bx4rd3vkz0dlyr4r519")))
+       #:hash (base32 "0z16spylp4s1l7lx7p6v11fi3yjq92r189hyf1h197h52xwmdjlh")))
      (build-system rails-build-system)
      (arguments
       `(#:phases
