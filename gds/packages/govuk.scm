5fc6f75a0ee86e45d367800e63b11c30bd157439
Package Updater <govuk-guix-package-updater@digital.cabinet-office.gov.uk>
2017-10-19 14:01:16 +0000

    Update short-url-manager from release_126 to release_127

--- gds/packages/govuk.scm
+++ gds/packages/govuk.scm
@@ -1044,12 +1044,12 @@ content, as well as broadcasting changes to a message queue.")
     (hash (base32 "1l33lx8qbhw0cg7dzwhvxwfrzkflvqxr7qwlvygpnpqqad2d87hn")))
    (package
      (name "short-url-manager")
-     (version "release_126")
+     (version "release_127")
      (source
       (github-archive
        #:repository name
        #:commit-ish version
-       #:hash (base32 "0lmp63wkq0dwswvzy9i36cwld8adpc7aw1s059wlg9yhck5g6mia")))
+       #:hash (base32 "1j61ykvqngcb7bkh6az1i3s7r6x62ryyx7d7nbk2j7icksjwmj6h")))
      (build-system rails-build-system)
      (arguments `(#:precompile-rails-assets? #f ;; Asset precompilation fails
                   #:ruby ,ruby-2.3))
