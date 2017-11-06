6b325ada0f1569a3326cb0f3b774ed5933873d17
Package Updater <govuk-guix-package-updater@digital.cabinet-office.gov.uk>
2017-11-06 16:32:08 +0000

    Update frontend from release_2747 to release_2748

--- gds/packages/govuk.scm
+++ gds/packages/govuk.scm
@@ -463,12 +463,12 @@ proxies requests to some upstream")
     (hash (base32 "1pngyfyiw4jbbz8yvf10f7nzynaxgbac3q108wydzc6phijpgw6v")))
    (package
      (name "frontend")
-     (version "release_2747")
+     (version "release_2748")
      (source
       (github-archive
        #:repository name
        #:commit-ish version
-       #:hash (base32 "0izgp47308iv1s4s0x6yqbjg8fk8qc0mzs3sy7rgh12d0gmx8b0h")))
+       #:hash (base32 "10k7x37kbny38s9sdavvwip9rs3cri8h5sdp51jcdkl55mw4nwf3")))
      (build-system rails-build-system)
      (arguments `(#:ruby ,ruby-2.3))
      (synopsis "")
