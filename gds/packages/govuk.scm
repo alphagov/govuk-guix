99cafb0b4b114445d409b0b0a2a8ddee12d38cbe
Package Updater <govuk-guix-package-updater@digital.cabinet-office.gov.uk>
2017-11-07 14:07:39 +0000

    Update content-tagger from release_568 to release_569

--- gds/packages/govuk.scm
+++ gds/packages/govuk.scm
@@ -300,12 +300,12 @@ proxies requests to some upstream")
     (hash (base32 "1gi159rdjm7cjh41bl7swjcywkkm17yz12r738m3jyx51qvn30ls")))
    (package
      (name "content-tagger")
-     (version "release_568")
+     (version "release_569")
      (source
       (github-archive
        #:repository name
        #:commit-ish version
-       #:hash (base32 "0zqg54r9j4c1bxw9n98jx46qff7n7mcbw6jmw62l7pj9q5xgy5kd")))
+       #:hash (base32 "0razhbs655gsi5bghz5p8gj6j6qlmv2r34q1fj18awx6n45ypb6l")))
      (build-system rails-build-system)
      (synopsis "")
      (description "")
