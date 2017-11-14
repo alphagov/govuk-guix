0f85dd03005bb5681d3353086aa53b619e87af15
Package Updater <govuk-guix-package-updater@digital.cabinet-office.gov.uk>
2017-11-14 10:41:33 +0000

    Update government-frontend from release_492 to release_493

--- gds/packages/govuk.scm
+++ gds/packages/govuk.scm
@@ -482,12 +482,12 @@ proxies requests to some upstream")
     (hash (base32 "1hkb8rsdnz9fck72x4p7vn60vz48aqw45dcvkh3h7ylv5r295qc8")))
    (package
      (name "government-frontend")
-     (version "release_492")
+     (version "release_493")
      (source
       (github-archive
        #:repository name
        #:commit-ish version
-       #:hash (base32 "0v3qra3cp1yi6qbg9m9vy95005mbw903liqjk3knw6hrvrxdwi4d")))
+       #:hash (base32 "1jnkwa8464yf2ak0hlwbaq41g5wbplwh621s7sjq9jjcr6b9rsqf")))
      (build-system rails-build-system)
      (arguments
       `(#:phases
