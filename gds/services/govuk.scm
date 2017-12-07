9545d9c37b3e0af8e366bf2a22539aeb909be813
Christopher Baines <christopher.baines@digital.cabinet-office.gov.uk>
2017-12-07 08:00:36 +0000

    Remove the Need API signon authorisation from Whitehall
    The Need API is no longer used.

--- gds/services/govuk.scm
+++ gds/services/govuk.scm
@@ -1833,10 +1833,6 @@
             (cons
              (signon-authorisation
               (application-name "Publishing API"))
-             '("signin"))
-            (cons
-             (signon-authorisation
-              (application-name "Need API"))
              '("signin")))))
          (sidekiq-config
           (file "config/sidekiq.yml"))
