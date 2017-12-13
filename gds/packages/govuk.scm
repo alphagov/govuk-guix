cf26a83f1a6f5ad094f5b3617fe58087238d59a3
Package Updater <govuk-guix-package-updater@digital.cabinet-office.gov.uk>
2017-12-13 15:47:50 +0000

    Update asset-manager from release_194 to release_195

--- gds/packages/govuk.scm
+++ gds/packages/govuk.scm
@@ -53,12 +53,12 @@
     (hash (base32 "1j3ihiwmlcs2z1cy09dls6mq5m524kxbm0qmmjsvpdfn68sgdwqr")))
    (package
      (name "asset-manager")
-     (version "release_194")
+     (version "release_195")
      (source
       (github-archive
        #:repository name
        #:commit-ish version
-       #:hash (base32 "0hwwq9mhpqmnzbipiq8wwx6r13lpy5jz2p0x4nr3b8zavayg5c47")))
+       #:hash (base32 "1w9szrjm2vvyy74isfnpcfjfpdxbbqmvmxikwpcpk6v0fkggipil")))
      (build-system rails-build-system)
      (inputs
       `(("govuk_clamscan"
