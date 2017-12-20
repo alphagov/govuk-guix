48b6136b05435b4a712d98ee0cb1ca51bc9ced40
Package Updater <govuk-guix-package-updater@digital.cabinet-office.gov.uk>
2017-12-20 15:09:29 +0000

    Update licence-finder from release_300 to release_301

--- gds/packages/govuk.scm
+++ gds/packages/govuk.scm
@@ -641,15 +641,15 @@ service setup.")
 (define-public licence-finder
   (package-with-bundler
    (bundle-package
-    (hash (base32 "11syagsk0c0243a6nmvpnf5yz5dz7q7zsilf6yfzahybjygrh9z7")))
+    (hash (base32 "05kaanrif877asdkc822lp3flrp61w4kk71hvpgmbjqwawp3fgyb")))
    (package
      (name "licence-finder")
-     (version "release_300")
+     (version "release_301")
      (source
       (github-archive
        #:repository name
        #:commit-ish version
-       #:hash (base32 "0z7x2mxk09wqxa4dxbghsz3450hq9mvp3s2akczn23gfl9j8kvsj")))
+       #:hash (base32 "1n9drcyk7hvkwk269gi7pihk7mbr8fyz5163nj04s4780gr6an7k")))
      (build-system rails-build-system)
      (arguments `(#:ruby ,ruby-2.3))
      (synopsis "")
