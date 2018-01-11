3e3f7049d7478797e5cc669f24e939e2cfa615d2
Package Updater <govuk-guix-package-updater@digital.cabinet-office.gov.uk>
2018-01-11 10:20:49 +0000

    Update finder-frontend from release_361 to release_362

--- gds/packages/govuk.scm
+++ gds/packages/govuk.scm
@@ -425,15 +425,15 @@ proxies requests to some upstream")
 (define-public finder-frontend
   (package-with-bundler
    (bundle-package
-    (hash (base32 "12mydvnp903fc3423d88azr8mckbkymmc0avlqk4ijx3s7y2qhg2")))
+    (hash (base32 "0wrdn8xqz48zn8jjdq7kvriiql5m9a62yn2y2x2jxvr9lcl0k8dg")))
    (package
      (name "finder-frontend")
-     (version "release_361")
+     (version "release_362")
      (source
       (github-archive
        #:repository name
        #:commit-ish version
-       #:hash (base32 "0c2chxxbv1nkxrjlq072f04pa0m1hvnwhma4nv96c8plbx5599vg")))
+       #:hash (base32 "0w3llf9a2k8g25fgfp638a9nibrd7icgsz1n949z5s7bsvllhp3f")))
      (build-system rails-build-system)
      (synopsis "")
      (description "")
