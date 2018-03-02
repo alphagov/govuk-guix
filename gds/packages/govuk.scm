730b33774e1491293e5025c3f20699c16a37a015
Package Updater <govuk-guix-package-updater@digital.cabinet-office.gov.uk>
2018-03-02 15:24:50 +0000

    Update asset-manager from release_263 to release_264

--- gds/packages/govuk.scm
+++ gds/packages/govuk.scm
@@ -56,12 +56,12 @@ GovukAdminTemplate.environment_label = ENV.fetch('GOVUK_ADMIN_TEMPLATE_ENVIRONME
     (hash (base32 "01waz1v7718i7l7iv8mn7m5rl2dd2gcqqhfx18bhzdk8vyf7xx2s")))
    (package
      (name "asset-manager")
-     (version "release_263")
+     (version "release_264")
      (source
       (github-archive
        #:repository name
        #:commit-ish version
-       #:hash (base32 "04sqaj5j9m80rrcv26crqkf3syx5gwfk27f01kgdp393ns6hppmm")))
+       #:hash (base32 "12gnsibffk7frk22pp767hdwqm3m0lwg2hayk6ids5aa7nnxdpab")))
      (build-system rails-build-system)
      (inputs
       `(("govuk_clamscan"
