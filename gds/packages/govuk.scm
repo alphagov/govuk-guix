7d231887dc0eb5b5d86dffbf236e0d9527baab2c
Package Updater <govuk-guix-package-updater@digital.cabinet-office.gov.uk>
2018-03-05 12:32:50 +0000

    Update content-performance-manager from release_419 to release_420

--- gds/packages/govuk.scm
+++ gds/packages/govuk.scm
@@ -273,12 +273,12 @@ proxies requests to some upstream")
     (hash (base32 "1na2yzg1sngrc82zdrd07qzmg9h6xcxhjcm2hs36w70qy7lca070")))
    (package
      (name "content-performance-manager")
-     (version "release_419")
+     (version "release_420")
      (source
       (github-archive
        #:repository name
        #:commit-ish version
-       #:hash (base32 "0df04ndlc9rx9a4a7zdcpy1swax8vjzqbcbj9215l7mdrk7fvad6")))
+       #:hash (base32 "0vz9z1yg9khd2adr2h85fj8jy29pyf151lggmznp4j4rrzsrr6g2")))
      (build-system rails-build-system)
      (arguments
       `(#:phases
