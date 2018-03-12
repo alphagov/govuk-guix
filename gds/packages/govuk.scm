b7d7e5337c23c8d422b9918693b4de9e6bfdbbae
Package Updater <govuk-guix-package-updater@digital.cabinet-office.gov.uk>
2018-03-12 08:51:12 +0000

    Update email-alert-service from release_132 to release_133

--- gds/packages/govuk.scm
+++ gds/packages/govuk.scm
@@ -390,15 +390,15 @@ proxies requests to some upstream")
 (define-public email-alert-service
   (package-with-bundler
    (bundle-package
-    (hash (base32 "12zqvj75r24ibjzr84l66dcp6nz21rvxffbpwwnywz1pvr45blm0")))
+    (hash (base32 "1vnry7nxmj6kl4hvln8rjhnysydafj23c94zxi3zxd1h9lmk86dr")))
    (package
      (name "email-alert-service")
-     (version "release_132")
+     (version "release_133")
      (source
       (github-archive
        #:repository name
        #:commit-ish version
-       #:hash (base32 "11k777wph2v70p89a99kqaqpii22pz94zggl44rzsvwih7bjhr82")))
+       #:hash (base32 "18j8wphgf2hjl5wjfff2nhn3vs5cwspngz3x8ksiwpfc9lf9lyg6")))
      (build-system gnu-build-system)
      (inputs
       `(("ruby" ,ruby)))
