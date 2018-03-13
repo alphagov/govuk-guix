44a4cebf2f27a950faf57e8e96242f95aa361344
Christopher Baines <christopher.baines@digital.cabinet-office.gov.uk>
2018-03-13 08:42:15 +0000

    Add (gds data tar-archive) module

---
+++ gds/data/tar-archive.scm
@@ -0,0 +1,55 @@
+(define-module (gds data tar-archive)
+  #:use-module (ice-9 match)
+  #:use-module (guix gexp)
+  #:use-module (guix store)
+  #:use-module (guix packages)
+  #:use-module (guix records)
+  #:use-module (guix monads)
+  #:use-module (gnu packages base)
+  #:use-module (gnu packages compression)
+  #:export (<tar-archive>
+            tar-archive
+            tar-archive?
+            tar-archive-name
+            tar-archive-contents))
+
+(define-record-type* <tar-archive>
+  tar-archive make-tar-archive
+  tar-archive?
+  (name     tar-archive-name)
+  (contents tar-archive-contents))
+
+(define-gexp-compiler (tar-extract-compiler
+                       (tar-extract <tar-archive>) system target)
+  (match tar-extract
+    (($ <tar-archive> name contents)
+     (mlet %store-monad ((guile (package->derivation (default-guile) system)))
+
+       (define inputs (list gzip))
+
+       (define build
+         (with-imported-modules `((guix build utils))
+           #~(let* ((tar-command
+                     (list
+                      (string-append #$tar "/bin/tar")
+                      "--create"
+                      "--file"
+                      #$output
+                      #$contents)))
+               (use-modules (srfi srfi-1)
+                            (ice-9 ftw)
+                            (guix build utils))
+               (set-path-environment-variable
+                "PATH" '("bin" "sbin") '#+inputs)
+
+               (simple-format #t "\nrunning ~A\n" (string-join tar-command))
+               (force-output)
+               (let ((status (apply system* tar-command)))
+                 (unless (zero? status)
+                   (exit 1))))))
+
+       (gexp->derivation name build
+                         #:system system
+                         #:local-build? #t
+                         #:recursive? #t
+                         #:guile-for-build guile)))))
