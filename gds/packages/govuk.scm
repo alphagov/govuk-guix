8787ed3a3c3974131008cb89730adb065866386f
Package Updater <govuk-guix-package-updater@digital.cabinet-office.gov.uk>
2018-04-05 12:26:29 +0100

    Update collections-publisher from release_374 to release_376

--- gds/packages/govuk.scm
+++ gds/packages/govuk.scm
@@ -218,12 +218,12 @@ proxies requests to some upstream")
     (hash (base32 "0v0wndrvp2bn8z2qv7v67bnwlxsxi7n7xj786s6k4p14ax7a52dx")))
    (package
      (name "collections-publisher")
-     (version "release_374")
+     (version "release_376")
      (source
       (github-archive
        #:repository name
        #:commit-ish version
-       #:hash (base32 "05s7n5nj4al0h1kk6vbm61d81qvxc5c7hz2rcgikc46rx38slayg")))
+       #:hash (base32 "1q7ad3f075hhj2i7q3p3lmbl79rsrvm4sl9hjjhkpz7r3wpg2w9z")))
      (build-system rails-build-system)
      (arguments
       `(#:phases
