77ce87fcf01c9235e4f409c11f30758e395cdaca
Package Updater <govuk-guix-package-updater@digital.cabinet-office.gov.uk>
2018-05-11 09:37:00 +0100

    Update publisher from release_1963 to release_1964

--- gds/packages/govuk.scm
+++ gds/packages/govuk.scm
@@ -950,12 +950,12 @@ service setup.")
     (hash (base32 "1a1m03q775dvrl38vv66vf0wn029xy683chrq11r885f3zxwcdjc")))
    (package
      (name "publisher")
-     (version "release_1963")
+     (version "release_1964")
      (source
       (github-archive
        #:repository name
        #:commit-ish version
-       #:hash (base32 "1pk7ly4z4zw4i6pbn043895lnkbc1m57xbfl8xwha4461084i39z")))
+       #:hash (base32 "02rv68yvyzk6nw4zfqr9kgwl82n65cprfmzkygc8yihrv2cjh5sm")))
      (build-system rails-build-system)
      (arguments
       `(#:phases
