50c9837bf8b5908fc4564b869d4dce81e15105a7
Package Updater <govuk-guix-package-updater@digital.cabinet-office.gov.uk>
2018-06-01 09:37:41 +0100

    Update calculators from release_307 to release_308

--- gds/packages/govuk.scm
+++ gds/packages/govuk.scm
@@ -158,15 +158,15 @@ proxies requests to some upstream")
 (define-public calculators
   (package-with-bundler
    (bundle-package
-    (hash (base32 "1b9zpj3axy6c1ppwmwwiqdh4nsqbrkz2jjpdwrz9id211qkgw922")))
+    (hash (base32 "0z05f3irblg4l3d2qmj07p1i4hq5qn2rvi97yyjvsjcil5jl4wm9")))
    (package
      (name "calculators")
-     (version "release_307")
+     (version "release_308")
      (source
       (github-archive
        #:repository name
        #:commit-ish version
-       #:hash (base32 "1dgkyf656iy8wdb6v7mr23php4s74xd97ksx3hvyvxpqqw5rfrnd")))
+       #:hash (base32 "0173d19mn2k45zbx4jzs9fzf7nm2fi2l9m0gxcalq0wckwn71ji2")))
      (build-system rails-build-system)
      (synopsis "Calculators provides the Child benefit tax calculator")
      (description "")
