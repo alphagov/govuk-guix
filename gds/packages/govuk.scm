b0c3ee9af6c86cfb11e85e49f39af3a70de3bb99
Package Updater <govuk-guix-package-updater@digital.cabinet-office.gov.uk>
2018-06-28 08:16:11 +0100

    Update calendars from release_549 to release_550

--- gds/packages/govuk.scm
+++ gds/packages/govuk.scm
@@ -177,15 +177,15 @@ proxies requests to some upstream")
 (define-public calendars
   (package-with-bundler
    (bundle-package
-    (hash (base32 "16gicva170rs621xwm5y1fw26bm37x9i3rwywlsyjinwxr5s224j")))
+    (hash (base32 "0152xvnzhszjs139r8rvflcwipp2hysd042dv3p06yl2pnzvki2a")))
    (package
      (name "calendars")
-     (version "release_549")
+     (version "release_550")
      (source
       (github-archive
        #:repository name
        #:commit-ish version
-       #:hash (base32 "1bvzqw5as4p6pprh1vzab4v3b74dmh40h739w64dhf41wvczn60h")))
+       #:hash (base32 "0xapi1yjpg9l3g7dphrwpa5d1a2g2d0mpr45v7qakg7sipbn9m3a")))
      (build-system rails-build-system)
      (synopsis "Serves calendars on GOV.UK")
      (description "")
