03b19536ebeef6d830c2f934acddbc70938592b4
Package Updater <govuk-guix-package-updater@digital.cabinet-office.gov.uk>
2018-06-29 07:48:44 +0100

    Update signon from release_1048 to release_1049

--- gds/packages/govuk.scm
+++ gds/packages/govuk.scm
@@ -1295,16 +1295,16 @@ content, as well as broadcasting changes to a message queue.")
 (define-public signon
   (package-with-bundler
    (bundle-package
-    (hash (base32 "0rql3bal71s734cih3wybi433cnzz6bqakvix13wmay3sypqgph0"))
+    (hash (base32 "1l7dhnlh6fmcayagkq45kyv2i7mbyyv0247qrgybzql7h04xzk46"))
     (without '("development" "test")))
    (package
      (name "signon")
-     (version "release_1048")
+     (version "release_1049")
      (source
       (github-archive
        #:repository name
        #:commit-ish version
-       #:hash (base32 "1id42dd7dwgmz3gni1gdsns86vapq49bs9qw4niz0z5arzi9yxcp")))
+       #:hash (base32 "1kwxmwilsw6rn1r3sgh3slpmk4nn8f2ghhwh57knbp08c9v4xrga")))
      (build-system rails-build-system)
      (arguments
       `(#:phases
