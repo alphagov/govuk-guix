cb8c83d062c51a72b4f2654aeaa025bc9a63ea99
Package Updater <govuk-guix-package-updater@digital.cabinet-office.gov.uk>
2018-07-11 13:21:11 +0100

    Update local-links-manager from release_237 to release_239

--- gds/packages/govuk.scm
+++ gds/packages/govuk.scm
@@ -820,15 +820,15 @@ service setup.")
 (define-public local-links-manager
   (package-with-bundler
    (bundle-package
-    (hash (base32 "19wvqjwbkfab9wnixywckn6lxp5fgphb4b9qy4ppv76qak4mk1p3")))
+    (hash (base32 "0jd59lr1kddr3nfy6bryl0kj5pxp6ci4m7jvz4bzyys54073g03c")))
    (package
      (name "local-links-manager")
-     (version "release_237")
+     (version "release_239")
      (source
       (github-archive
        #:repository name
        #:commit-ish version
-       #:hash (base32 "0zjnizgkp1fd2fwdl6i1li81mc2kynjnamb7ldnnisnl9jq3wwdz")))
+       #:hash (base32 "1sr7vzz0q8ph6arwjyyjg87khmwd41nbxigxbm2dpjvmn6c3zyga")))
      (build-system rails-build-system)
      (inputs
       `(;; hostname is needed by the redis-lock gem
