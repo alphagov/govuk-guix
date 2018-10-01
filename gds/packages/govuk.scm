25e316fdd111cc7f010f99443980cbf3b1505960
Package Updater <govuk-guix-package-updater@digital.cabinet-office.gov.uk>
2018-10-01 16:16:37 +0100

    Update authenticating-proxy from release_97 to release_98

--- gds/packages/govuk.scm
+++ gds/packages/govuk.scm
@@ -116,15 +116,15 @@ GovukAdminTemplate.environment_label = ENV.fetch('GOVUK_ADMIN_TEMPLATE_ENVIRONME
 (define-public authenticating-proxy
   (package-with-bundler
    (bundle-package
-    (hash (base32 "15mvqa877sbh8shaq2b9wyb3xip4bz5xlygqcm3zr380pfgy9l0y")))
+    (hash (base32 "108dqqqdnchpbd8kdx97r7yivbmh5ypi74hgca0yx11ap8s9lf63")))
    (package
      (name "authenticating-proxy")
-     (version "release_97")
+     (version "release_98")
      (source
       (github-archive
        #:repository name
        #:commit-ish version
-       #:hash (base32 "0sijnygb85cf0ahsfljg6r90alsjwxm53275bvq15bgl6h8mdg4z")))
+       #:hash (base32 "1azxda553fv4xph18fhxxddh427w81ndj5372giw0sn4zym4xxkv")))
      (build-system rails-build-system)
      (arguments
       `(#:precompile-rails-assets? #f
