b483515535f522ff7799f2f07c0eb4201b88580c
Package Updater <govuk-guix-package-updater@digital.cabinet-office.gov.uk>
2018-10-29 16:45:32 +0000

    Update content-publisher from release_367 to release_369

--- gds/packages/govuk.scm
+++ gds/packages/govuk.scm
@@ -359,12 +359,12 @@ proxies requests to some upstream")
     (hash (base32 "12mgl9aj0xzizlr532w0b39w5c5dwwsa8zl4hbkdmk96vq7pd1qv")))
    (package
      (name "content-publisher")
-     (version "release_367")
+     (version "release_369")
      (source
       (github-archive
        #:repository name
        #:commit-ish version
-       #:hash (base32 "0wj8rfxr4p4ng04g0fw1dzn04cm3j9z1lxsbaxqynmd9drcwjwhj")))
+       #:hash (base32 "126fbnqf05g6vylbw46pjjrpx60wfwpqk0fgf5b5a8rp23wxkvsl")))
      (build-system rails-build-system)
      (arguments
       `(#:phases
