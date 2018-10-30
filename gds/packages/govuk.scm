8ada5dda6c11c7e1e79e3bb590d67406a3f24b0c
Package Updater <govuk-guix-package-updater@digital.cabinet-office.gov.uk>
2018-10-30 13:47:32 +0000

    Update content-data-admin from release_153 to release_154

--- gds/packages/govuk.scm
+++ gds/packages/govuk.scm
@@ -314,12 +314,12 @@ proxies requests to some upstream")
     (hash (base32 "16gl0xg81alv0fggg9iki1lrnclhy1g4dyklc0nch26glfp5j9v7")))
    (package
      (name "content-data-admin")
-     (version "release_153")
+     (version "release_154")
      (source
       (github-archive
        #:repository name
        #:commit-ish version
-       #:hash (base32 "1b2dz1d744ahapy4dv753bf1x0gn1cwx1slbmrhcgfq4rxb2n45s")))
+       #:hash (base32 "0fwmiifyx8s91jxmwssn5s5c9nl47s0lykw2dpr6qml6ddf6nvp6")))
      (build-system rails-build-system)
      (synopsis "A front end for the data warehouse")
      (description "")
