1c875a98b380dd8c444bbfa468b2624a2934f212
Christopher Baines <christopher.baines@digital.cabinet-office.gov.uk>
2018-12-16 21:23:21 +0000

    Add a couple more transformation related record types

--- gds/data/transformations.scm
+++ gds/data/transformations.scm
@@ -10,7 +10,20 @@
             data-transformation?
             data-transformation-output-name
             data-transformation-output-description
-            data-transformation-operation))
+            data-transformation-operation
+
+            <multi-output-data-transformation>
+            multi-output-data-transformation
+            multi-output-data-transformation?
+            multi-output-data-transformation-name
+            multi-output-data-transformation-outputs
+            multi-output-data-transformation-operation
+
+            <gexp-output-alias>
+            gexp-output-alias
+            gexp-output-alias
+            gexp-output-alias-gexp
+            gexp-output-alias-output))
 
 (define-record-type* <data-transformation>
   data-transformation make-data-transformation
@@ -30,3 +43,37 @@
      (gexp->derivation output-name operation
                        #:system system))))
 
+(define-record-type* <multi-output-data-transformation>
+  multi-output-data-transformation make-multi-output-data-transformation
+  multi-output-data-transformation?
+  (name      multi-output-data-transformation-name)
+  (outputs   multi-output-data-transformation-outputs)
+  (operation multi-output-data-transformation-operation))
+
+(define-gexp-compiler (multi-output-data-transformation-compiler
+                       (multi-output-data-transformation
+                        <multi-output-data-transformation>)
+                       system target)
+  (match multi-output-data-transformation
+    (($ <multi-output-data-transformation> name outputs operation)
+
+     (gexp->derivation name
+                       operation
+                       #:system system))))
+
+(define-record-type* <gexp-output-alias>
+  gexp-output-alias make-gexp-output-alias
+  gexp-output-alias?
+  (gexp   gexp-output-alias-gexp)
+  (output gexp-output-alias-output))
+
+(define-gexp-compiler (gexp-output-alias-compiler
+                       (gexp-output-alias <gexp-output-alias>)
+                       system target)
+  (match gexp-output-alias
+    (($ <gexp-output-alias> aliased-gexp output-name)
+
+     (gexp->derivation (string-append "output-alias-" output-name)
+                       #~(symlink (ungexp aliased-gexp output-name)
+                                  #$output)
+                       #:system system))))
