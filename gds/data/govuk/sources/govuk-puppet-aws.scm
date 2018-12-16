d55c48716749536fbdd81810d72737f10a826f7e
Christopher Baines <christopher.baines@digital.cabinet-office.gov.uk>
2018-12-16 21:45:42 +0000

    Add initial processing of PostgreSQL extracts
    Create a additional extract using the directory format from
    pg_dump. Additionally, for the Publishing API and Content Tagger databases,
    create some additional smaller extracts.

--- gds/data/govuk/sources/govuk-puppet-aws.scm
+++ gds/data/govuk/sources/govuk-puppet-aws.scm
@@ -13,13 +13,18 @@
   #:use-module (guix download)
   #:use-module (guix packages)
   #:use-module (gnu packages guile)
+  #:use-module (gnu services)
   #:use-module (gds services govuk)
   #:use-module (gds services govuk signon)
   #:use-module (gds services govuk rummager)
+  #:use-module (gds services utils databases)
+  #:use-module (gds services utils databases postgresql)
   #:use-module (gds data data-source)
   #:use-module (gds data data-extract)
   #:use-module (gds data tar-extract)
+  #:use-module (gds data transformations)
   #:use-module (gds data transformations mongodb)
+  #:use-module (gds data transformations postgresql)
   #:use-module (gds data govuk sources govuk-puppet)
   #:export (govuk-puppet-aws-data-source))
 
@@ -86,6 +91,99 @@
             (priority . 0)))
          (directory? #t))))
 
+(define (publishing-api-variants)
+  (define truncate-expanded-links
+    '(;; This table is used as a cache, so just truncate it
+      "TRUNCATE expanded_links"))
+
+  (define delete-all-but-one-month-of-actions
+    '(;; actions are not very useful, so remove most of them
+      "CREATE TEMP TABLE tmp_actions AS SELECT * FROM actions WHERE created_at > (CURRENT_DATE - INTERVAL '1 months')"
+      "CREATE TEMP TABLE tmp_link_changes AS SELECT * FROM link_changes WHERE action_id IN (SELECT id FROM tmp_actions)"
+      "TRUNCATE actions CASCADE"
+      "INSERT INTO actions SELECT * FROM tmp_actions"
+      "DROP TABLE tmp_actions"
+      "INSERT INTO link_changes SELECT * FROM tmp_link_changes"
+      "DROP TABLE tmp_link_changes"))
+
+  (define delete-all-but-one-month-of-events
+    '("CREATE TEMP TABLE tmp_events AS SELECT * FROM events WHERE created_at > (CURRENT_DATE - INTERVAL '1 months')"
+      "TRUNCATE events"
+      "INSERT INTO events SELECT * FROM tmp_events"))
+
+  (define set-details-to-NULL-for-old-superseded-editions
+    '("UPDATE editions SET details = NULL WHERE id IN (SELECT id FROM editions WHERE state = 'superseded' AND updated_at < (CURRENT_DATE - INTERVAL '1 months'))"))
+
+  `((2 . ("no-expanded-links"
+          "Expanded_links table truncated."
+          ,truncate-expanded-links))
+    (3 . ("small"
+          "Expanded links table truncated, only 1 month of actions, events, link_changes and set details to NULL for superseded editions older than 1 month."
+          ,(append delete-all-but-one-month-of-actions
+                   delete-all-but-one-month-of-events
+                   set-details-to-NULL-for-old-superseded-editions)))))
+
+(define (postgresql-extract-variants)
+  `(("publishing_api_production" . ,(publishing-api-variants))
+    ;; This is mostly for testing, as content-tagger has a small database
+    ("content_tagger_production" .
+     ((2 . ("no-taxonomy-health-warnings"
+            "No taxonomy health warnings."
+            ("TRUNCATE taxonomy_health_warnings")))))))
+
+(define (postgresql-extract-plus-variants
+         base-extract database-connection-config variant-details)
+  (define transformation
+    (postgresql-multi-output-data-transformation
+     base-extract
+     database-connection-config
+     (map cdr variant-details)))
+
+  (cons base-extract
+        (map (match-lambda
+               ((priority name label sql)
+                (data-extract
+                 (inherit base-extract)
+                 (file (gexp-output-alias (gexp transformation)
+                                          (output name)))
+                 (variant-name name)
+                 (variant-label label)
+                 (variant-properties `((format . "directory")
+                                       (priority . ,priority)))
+                 (directory? #t))))
+             variant-details)))
+
+(define (generate-postgresql-extract-variants base-extracts)
+  (define (configure-service service)
+    (first
+     (map
+      (cut update-service-database-connection-config-for-environment
+        "production" <>)
+      (list service))))
+
+  (define (database-connection-config extract)
+    (find postgresql-connection-config?
+          (service-parameters
+           (configure-service
+            (find (lambda (service)
+                    (eq? (service-kind service)
+                         (first
+                          (data-extract-services extract))))
+                  govuk-services)))))
+
+  (append-map
+   (lambda (extract)
+     (postgresql-extract-plus-variants
+      extract
+      (database-connection-config extract)
+      (cons '(1 . ("directory"
+                   "Directory format dump."
+                   ()))
+            (or (assoc-ref (data-extract-name extract)
+                           (postgresql-extract-variants))
+                '()))))
+   base-extracts))
+
 (define (find-data-extracts backup-directory)
   (define (log message value)
     ;;(simple-format #t "~A: ~A\n" message value)
@@ -99,10 +197,11 @@
   (define (process-database-dir date database stat . children)
     (cond
      ((string=? database "postgresql")
-      (create-extracts-from-sql-dump-files postgresql-extracts
-                                           date
-                                           database
-                                           children))
+      (generate-postgresql-extract-variants
+       (create-extracts-from-sql-dump-files postgresql-extracts
+                                            date
+                                            database
+                                            children)))
 
      ((string=? database "mysql")
       (append-map (match-lambda*
@@ -191,7 +290,9 @@
 
     (define (create-data-extract filename services)
       (data-extract
-       (name filename)
+       (name (basename
+              (basename filename ".dump.xz")
+              ".dump.gz"))
        (file (local-file
               (string-join
                `(,backup-directory
@@ -205,7 +306,11 @@
        (datetime (string->date date "~Y-~m-~d"))
        (database database)
        (services services)
-       (data-source govuk-puppet-aws-data-source)))
+       (data-source govuk-puppet-aws-data-source)
+       (variant-name "plain")
+       (variant-label "Plain compressed SQL")
+       (variant-properties '((format . "plain")
+                             (priority . 0)))))
 
     (filter-map (match-lambda
                   ((extract-prefix . services)
