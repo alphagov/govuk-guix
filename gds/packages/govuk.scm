b32995d6e48b99d45e926a280d4223167e9bab68
Package Updater <govuk-guix-package-updater@digital.cabinet-office.gov.uk>
2019-03-08 17:35:00 +0000

    Update content-publisher from release_758 to release_759

--- gds/packages/govuk.scm
+++ gds/packages/govuk.scm
@@ -365,12 +365,12 @@ proxies requests to some upstream")
     (hash (base32 "0sg8lak4m3z9gg614gkgr5i9i6rvc9fj6a23p5a4g2pfa4a4dz6i")))
    (package
      (name "content-publisher")
-     (version "release_758")
+     (version "release_759")
      (source
       (github-archive
        #:repository name
        #:commit-ish version
-       #:hash (base32 "1wkr5b0dmfc6lzqwxggl8gwrs7cfh8l7bfkdisr8g5f18kg20p2l")))
+       #:hash (base32 "1xci5rgqkh7mnm6v1h97a2kda79s5w2cchmn5ivgnzlnigjjcnyp")))
      (build-system rails-build-system)
      (arguments
       `(;; TODO: Asset precompilation is now failing, due to npm not
