526c53c53dae6d1fc0c7bb37100bfc7971189d3b
Package Updater <govuk-guix-package-updater@digital.cabinet-office.gov.uk>
2019-03-22 17:36:23 +0000

    Update content-store from release_853 to release_854

--- gds/packages/govuk.scm
+++ gds/packages/govuk.scm
@@ -399,15 +399,15 @@ proxies requests to some upstream")
 (define-public content-store
   (package-with-bundler
    (bundle-package
-    (hash (base32 "0zjxl62zg6qsp5drqapzbvkgxr872y4wb2b5vsrhp435znhllf7p")))
+    (hash (base32 "13dxf4jlvdh9dgh4qpzpj6i32v6hvhir29vnnshv2xr325wzp7wi")))
    (package
      (name "content-store")
-     (version "release_853")
+     (version "release_854")
      (source
       (github-archive
        #:repository name
        #:commit-ish version
-       #:hash (base32 "0475avqrawc15kdi1ywgjm2zzqldb3acgkgq808k6plqpr4i0amv")))
+       #:hash (base32 "033wicdxhyn1xmmhi43hjzrb8isxi5nc926xdhhmiyhh2zcldmry")))
      (build-system rails-build-system)
      (arguments '(#:precompile-rails-assets? #f))
      (synopsis "")
