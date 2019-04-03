440ca6cab6c7e07c44c7a3600dfdb2ab3dfe0982
Package Updater <govuk-guix-package-updater@digital.cabinet-office.gov.uk>
2019-04-03 13:35:09 +0100

    Update finder-frontend from release_897 to release_898

--- gds/packages/govuk.scm
+++ gds/packages/govuk.scm
@@ -554,12 +554,12 @@ proxies requests to some upstream")
     (hash (base32 "03z04fb28n3xp5izn6j0g8papkhsw1vm436l3ymgjvjlv53l4yxi")))
    (package
      (name "finder-frontend")
-     (version "release_897")
+     (version "release_898")
      (source
       (github-archive
        #:repository name
        #:commit-ish version
-       #:hash (base32 "02iwkf3wjdp6ish5wfjnakm81lkln4dpq27dv48aflpcqni6xab3")))
+       #:hash (base32 "0yl6b1xm0qq92iklmsmadp7h2nidcj3xskgikxj2nv5bimqk9naj")))
      (build-system rails-build-system)
      (arguments
       '(;; jasmine-rails seems to get annoyed if it's configuration
