bcdfeb727936a9ad568922d59e147ca9495fb8d0
Package Updater <govuk-guix-package-updater@digital.cabinet-office.gov.uk>
2019-06-28 16:15:01 +0100

    Update content-data-admin from release_534 to release_535

--- gds/packages/govuk.scm
+++ gds/packages/govuk.scm
@@ -330,12 +330,12 @@ proxies requests to some upstream")
     (hash (base32 "1lj0r8j7j3ldgcc527cl02pz9nr39zgk2qn8cg1bb819zf5w7hmw")))
    (package
      (name "content-data-admin")
-     (version "release_534")
+     (version "release_535")
      (source
       (github-archive
        #:repository name
        #:commit-ish version
-       #:hash (base32 "12a3v3lxa89hp67s7j1zl01g4x02nbm2jh5f6hq3v7xkrvb14y34")))
+       #:hash (base32 "1iwwjirjcgzslwlykdl7sb667jbv40dq6pc545ls7ichyyarlmzl")))
      (build-system rails-build-system)
      (native-inputs
       `(("chromium" ,ungoogled-chromium)))
