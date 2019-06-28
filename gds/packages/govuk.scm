0a55b8e76cf857b72c82d928774a3baa78650e55
Package Updater <govuk-guix-package-updater@digital.cabinet-office.gov.uk>
2019-06-28 11:53:06 +0100

    Update whitehall from release_14132 to release_14133

--- gds/packages/govuk.scm
+++ gds/packages/govuk.scm
@@ -1819,12 +1819,12 @@ content, as well as broadcasting changes to a message queue.")
     (hash (base32 "0kbibsvl6bapk1p1wkzjf3y8lkkza27zvc4rh7bk47kj6hqgyi1i")))
    (package
      (name "whitehall")
-     (version "release_14132")
+     (version "release_14133")
      (source
       (github-archive
        #:repository name
        #:commit-ish version
-       #:hash (base32 "162hmmv0ah0w354iv418cf0vw0zcypdpqh4xa50air4dd2d3mmds")))
+       #:hash (base32 "0gky99aw1k75hqag1skjwjz8rayk1vszizxyjkmnwvfr050368fy")))
      (build-system rails-build-system)
      (inputs
       `(("node" ,node)
