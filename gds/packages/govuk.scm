176928c431b30f05835bf1ceee6b06443cfe6bb2
Package Updater <govuk-guix-package-updater@digital.cabinet-office.gov.uk>
2019-08-22 12:18:18 +0100

    Update email-alert-api from release_868 to release_870

--- gds/packages/govuk.scm
+++ gds/packages/govuk.scm
@@ -467,12 +467,12 @@ proxies requests to some upstream")
     (hash (base32 "0ggzbh1gc37d8jfcgbzg9mq3law19p6nidmy49qbhlxj14b18va7")))
    (package
      (name "email-alert-api")
-     (version "release_868")
+     (version "release_870")
      (source
       (github-archive
        #:repository name
        #:commit-ish version
-       #:hash (base32 "180b7962vp3g49yhsigk7mz75jqa7qh54a9kqi7fd975dilg6h8p")))
+       #:hash (base32 "1cjqn663ijv0q106qc5ngj9gqyly00wfi9izkn8rr85nsawxnq9a")))
      (build-system rails-build-system)
      (arguments
       `(#:precompile-rails-assets? #f
