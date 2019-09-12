294ee9118e9d9b57035486c417f659352ae5a6fe
Package Updater <govuk-guix-package-updater@digital.cabinet-office.gov.uk>
2019-09-12 17:38:55 +0100

    Update content-data-admin from release_570 to release_571

--- gds/packages/govuk.scm
+++ gds/packages/govuk.scm
@@ -327,15 +327,15 @@ proxies requests to some upstream")
 (define-public content-data-admin
   (package-with-bundler
    (bundle-package
-    (hash (base32 "0gnbkg8lkpb7c5v15nhcq1bs9iq263wxsczxcy6rk678szgdgzpm")))
+    (hash (base32 "0qw91bgywlgj0b1pwbwgajlcxwpi7xbxlmy71fa9nc9qj4mmxylz")))
    (package
      (name "content-data-admin")
-     (version "release_570")
+     (version "release_571")
      (source
       (github-archive
        #:repository name
        #:commit-ish version
-       #:hash (base32 "0lrrv6905pc9dqx0hk9c194d64kk4d2cyzg7dlbqdfydbp9rizbf")))
+       #:hash (base32 "14waq8p5hsimfss32ajldx5h5473wrn5aff1v4ngr40kaklvniyq")))
      (build-system rails-build-system)
      (native-inputs
       `(("chromium" ,ungoogled-chromium)))
