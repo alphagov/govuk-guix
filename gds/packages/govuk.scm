d47d5984708e9038b460e3abeffedd129adf049d
Package Updater <govuk-guix-package-updater@digital.cabinet-office.gov.uk>
2019-09-20 10:43:24 +0100

    Update support-api from release_323 to release_324

--- gds/packages/govuk.scm
+++ gds/packages/govuk.scm
@@ -1738,15 +1738,15 @@ content, as well as broadcasting changes to a message queue.")
 (define-public support-api
   (package-with-bundler
    (bundle-package
-    (hash (base32 "1rq21rkvjbcyncmh6c6js2x5b5jnhn66na4cqgsr2zj3sk2gqwah")))
+    (hash (base32 "1wqc5r3am4a1masf8lqrvmr0p68xyym191yxz0arfkqwh0iyi5af")))
    (package
      (name "support-api")
-     (version "release_323")
+     (version "release_324")
      (source
       (github-archive
        #:repository name
        #:commit-ish version
-       #:hash (base32 "10g7l19j56523z0dgfdf9dgqnpc7lv24fdx2ml72qsw1gzpxmkpq")))
+       #:hash (base32 "1ckfgfml2rssfll7kg6v3iav0dv8kxmcbakc2r60r3xdmqpxm9ih")))
      (build-system rails-build-system)
      (inputs
       `(;; hostname is needed by the redis-lock gem
