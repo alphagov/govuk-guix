9a750e6b37843c459f3071af10a31c0debaa20ca
Package Updater <govuk-guix-package-updater@digital.cabinet-office.gov.uk>
2019-10-24 10:31:43 +0100

    Update support from release_898 to release_899

--- gds/packages/govuk.scm
+++ gds/packages/govuk.scm
@@ -1707,15 +1707,15 @@ content, as well as broadcasting changes to a message queue.")
 (define-public support
   (package-with-bundler
    (bundle-package
-    (hash (base32 "135m6lyfg1rglh8f3pgdzx75jhmanh4h5pbpg4n29qrs9rw79sj8")))
+    (hash (base32 "1rxg1zw42sdn3aq10wk9ym066cgrzc43plkj75hwahgirygamgvd")))
    (package
      (name "support")
-     (version "release_898")
+     (version "release_899")
      (source
       (github-archive
        #:repository name
        #:commit-ish version
-       #:hash (base32 "0lrkymx2f54wwllbzncnvyr13ri0w1a5n7ycvwx9q0zhv6knbagd")))
+       #:hash (base32 "1wfrrh958z2163mcq99hs33677w96v85mspbnja44a4gy7a27yic")))
      (build-system rails-build-system)
      (arguments
       `(#:phases
