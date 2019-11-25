9691705a4f8e0e1579634851f7acd46858c1b09a
Package Updater <govuk-guix-package-updater@digital.cabinet-office.gov.uk>
2019-11-25 12:38:19 +0000

    Update content-data-api from release_1163 to release_1164

--- gds/packages/govuk.scm
+++ gds/packages/govuk.scm
@@ -358,12 +358,12 @@ proxies requests to some upstream")
     (hash (base32 "10dycfqmqy1c963dkggkd4abzf9kpgqm6mgi56xik9vjx60ywr14")))
    (package
      (name "content-data-api")
-     (version "release_1163")
+     (version "release_1164")
      (source
       (github-archive
        #:repository name
        #:commit-ish version
-       #:hash (base32 "0q669i0kfbap9r020fpmw7zbrz86pq23n7shmava8i0y2qin0ji5")))
+       #:hash (base32 "1glmaq7i9cgx5l6sxp0bhza1nzsap6iqlbmlvixxf0lqhric84i1")))
      (build-system rails-build-system)
      (arguments
       `(#:phases
