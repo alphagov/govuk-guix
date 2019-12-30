f44fcfac1c20e56e281d75d35431ddd3928dfe44
Package Updater <govuk-guix-package-updater@digital.cabinet-office.gov.uk>
2019-12-30 14:06:55 +0000

    Update specialist-publisher from release_1255 to release_1256

--- gds/packages/govuk.scm
+++ gds/packages/govuk.scm
@@ -1591,16 +1591,16 @@ content, as well as broadcasting changes to a message queue.")
 (define-public specialist-publisher
   (package-with-bundler
    (bundle-package
-    (hash (base32 "0padp805zmrcl70ymk3a7jfbnz0hfp7hzl4f47ms65vs7d241zq9"))
+    (hash (base32 "1d08xyqdxbhimvfjpg9hfzihir1qps8abgz02hzyy8m6yskghbk4"))
     (without '("development" "test")))
    (package
      (name "specialist-publisher")
-     (version "release_1255")
+     (version "release_1256")
      (source
       (github-archive
        #:repository name
        #:commit-ish version
-       #:hash (base32 "1sw05z16jz7kfsnqvl6wb805ms10ljh3bp49d1w5pcfbgmh3w5iq")))
+       #:hash (base32 "0k68blpqq4n8i07xljfkf5njwlvmvyjaysk4bmvazmcpn4ghqah7")))
      (build-system rails-build-system)
      (arguments
       `(#:phases
