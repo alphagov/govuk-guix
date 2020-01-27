1876cf2f63da63d77a0e31d630b35b54d4582b0b
Package Updater <govuk-guix-package-updater@digital.cabinet-office.gov.uk>
2020-01-27 11:18:59 +0000

    Update government-frontend from release_1332 to release_1333

--- gds/packages/govuk.scm
+++ gds/packages/govuk.scm
@@ -621,12 +621,12 @@ proxies requests to some upstream")
     (hash (base32 "1dvxfbx3bn8ld4nf90nw9ijmi9v99mzld23jsdghjj49qf818yr6")))
    (package
      (name "government-frontend")
-     (version "release_1332")
+     (version "release_1333")
      (source
       (github-archive
        #:repository name
        #:commit-ish version
-       #:hash (base32 "05zbdv0i6p58c6lc72pij9443j4daykiam9ayhc2y567579i23ma")))
+       #:hash (base32 "1q3nfm1n4djzjyr93n8m8i6xz1bl6faf47c75zaxclv6is0d3zj2")))
      (build-system rails-build-system)
      (arguments
       '(;; jasmine-rails seems to get annoyed if it's configuration
