549d5039092a26c8a0dcc028992dc969b3aa0efa
Package Updater <govuk-guix-package-updater@digital.cabinet-office.gov.uk>
2020-01-31 15:39:31 +0000

    Update manuals-publisher from release_1273 to release_1274

--- gds/packages/govuk.scm
+++ gds/packages/govuk.scm
@@ -988,12 +988,12 @@ service setup.")
     (hash (base32 "0v541642cmyi3c8fixvhadqws5f7hrjvgb825vyz5kh3f47r436i")))
    (package
      (name "manuals-publisher")
-     (version "release_1273")
+     (version "release_1274")
      (source
       (github-archive
        #:repository name
        #:commit-ish version
-       #:hash (base32 "1iqymgzl8ygsgq04dyav738wrh89rs14wi37zlg4zbk8p8rrbr0l")))
+       #:hash (base32 "1k8mf70cix0f3i5ifib8n0k0ga5vvxy56bxw2ryj1k4n7n96r40w")))
      (build-system rails-build-system)
      (arguments
       `(#:phases
