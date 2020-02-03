6e6f6981c732a062c901799db06ba0660bdf8397
Package Updater <govuk-guix-package-updater@digital.cabinet-office.gov.uk>
2020-02-03 10:39:23 +0000

    Update info-frontend from release_466 to release_467

--- gds/packages/govuk.scm
+++ gds/packages/govuk.scm
@@ -870,12 +870,12 @@ service setup.")
     (hash (base32 "1v49xrj19vinzkq44k7bzyj9siq41ln07w6a3mps9p1zvr7ds8vd")))
    (package
      (name "info-frontend")
-     (version "release_466")
+     (version "release_467")
      (source
       (github-archive
        #:repository name
        #:commit-ish version
-       #:hash (base32 "0fhz1hf12yy3q6jsl7ian3dv2agvjhw6hfhd975i5il4wvnsz568")))
+       #:hash (base32 "19bcmzsqy9fh5h0128zdiarmh21f8hdls6nzwhmssgycwbx9mnx0")))
      (build-system rails-build-system)
      (synopsis "")
      (description "")
