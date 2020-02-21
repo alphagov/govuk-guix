d66d8b3745a7819aab1a786950375c1bae24890f
Package Updater <govuk-guix-package-updater@digital.cabinet-office.gov.uk>
2020-02-21 11:16:58 +0000

    Update search-api from release_2449 to release_2450

--- gds/packages/govuk.scm
+++ gds/packages/govuk.scm
@@ -1377,15 +1377,15 @@ content, as well as broadcasting changes to a message queue.")
 (define-public search-api
   (package-with-bundler
    (bundle-package
-    (hash (base32 "0h3dv9bn9kkkh45s68hibw86m6walzhd7bkwqzd88znsd9yy5m17")))
+    (hash (base32 "1jw2rwg0m7fa754r0fl03z7m1g1ss7skzfrmjlrxcy1lg2lcr92p")))
    (package
      (name "search-api")
-     (version "release_2449")
+     (version "release_2450")
      (source
       (github-archive
        #:repository name
        #:commit-ish version
-       #:hash (base32 "0mj2hmbgqbakfckd0300whf0g1cp7csr1maigizw1vdmq9f66fwl")))
+       #:hash (base32 "14wy10x9dz2011wkfvkf37k8csrk9358000g4f9g4529n433yvnv")))
      (build-system rails-build-system)
      (arguments '(#:precompile-rails-assets? #f))
      (synopsis "Search API for GOV.UK")
