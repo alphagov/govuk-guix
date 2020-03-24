a00f95b383c0819aec69cca72e7f060647b08e32
Package Updater <govuk-guix-package-updater@digital.cabinet-office.gov.uk>
2020-03-24 17:24:21 +0000

    Update collections from release_1311 to release_1312

--- gds/packages/govuk.scm
+++ gds/packages/govuk.scm
@@ -211,12 +211,12 @@ proxies requests to some upstream")
     (hash (base32 "0zmgjapd8jvgx4qnlggn1lg4ns8nwagvpkpvlmns2bl3hsnfccb8")))
    (package
      (name "collections")
-     (version "release_1311")
+     (version "release_1312")
      (source
       (github-archive
        #:repository name
        #:commit-ish version
-       #:hash (base32 "17m5fn1kqpwmhc7cjbl0mc83qjdwl71zn4r5yz7m3j690syskl17")))
+       #:hash (base32 "03ai39f6f41yv030ly1h90yh3nqrrwf1djvq4xif4xn1rpcyx31l")))
      (build-system rails-build-system)
      (arguments
       '(;; jasmine-rails seems to get annoyed if it's configuration
