080125a664b70310b5f505ae33c4fad320b0e923
Package Updater <govuk-guix-package-updater@digital.cabinet-office.gov.uk>
2020-03-26 12:10:22 +0000

    Update static from release_3450 to release_3451

--- gds/packages/govuk.scm
+++ gds/packages/govuk.scm
@@ -1642,12 +1642,12 @@ content, as well as broadcasting changes to a message queue.")
     (hash (base32 "1bpsmfpl1h9p579ffmmxbz93ahz475r7mnyrjjzc5gj2yyj4zham")))
    (package
      (name "static")
-     (version "release_3450")
+     (version "release_3451")
      (source
       (github-archive
        #:repository name
        #:commit-ish version
-       #:hash (base32 "1a9vyyfkp6mqfmnk98hhg45r5lqmw54803gyfxzla4l47b7icz42")))
+       #:hash (base32 "07ay7gd2mj299519hbg49dphf4hwb0p4g30y4mqfjmnpmrn6jfw5")))
      (build-system rails-build-system)
      (arguments
       '(;; jasmine-rails seems to get annoyed if it's configuration
