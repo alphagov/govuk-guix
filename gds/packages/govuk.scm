54984441e130e2ce0a6a5d770344ac6a32a84892
Package Updater <govuk-guix-package-updater@digital.cabinet-office.gov.uk>
2020-04-15 10:03:01 +0100

    Update email-alert-api from release_1111 to release_1112

--- gds/packages/govuk.scm
+++ gds/packages/govuk.scm
@@ -435,12 +435,12 @@ proxies requests to some upstream")
     (hash (base32 "18mdmj7wvx86y04bk50mj1439wrwh53l5fkwvxvvpkn2dmr49mch")))
    (package
      (name "email-alert-api")
-     (version "release_1111")
+     (version "release_1112")
      (source
       (github-archive
        #:repository name
        #:commit-ish version
-       #:hash (base32 "0vsz1v4pl0p65m6kj6jpxyfrvb74l0fz308j31wlc7agagwwlp00")))
+       #:hash (base32 "0mlg156rncgxvc9v9dkzfjkjz89gqx91xhazjw6161w8c9w1l7j4")))
      (build-system rails-build-system)
      (arguments
       `(#:precompile-rails-assets? #f
