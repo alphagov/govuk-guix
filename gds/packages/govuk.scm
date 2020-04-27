2784b8d3e8ef46e472dcdfb62350225b36f84869
Package Updater <govuk-guix-package-updater@digital.cabinet-office.gov.uk>
2020-04-27 15:01:44 +0100

    Update publishing-api from release_1650 to release_1651

--- gds/packages/govuk.scm
+++ gds/packages/govuk.scm
@@ -1182,15 +1182,15 @@ data to use can be selected, and multiple backends are supported.")
 (define-public publishing-api
   (package-with-bundler
    (bundle-package
-    (hash (base32 "020jp95h640q4zfpswnapl5bd3lyx70nsvgjl41csbr4vwjh5i13")))
+    (hash (base32 "0s5538mwap8m9lqqp4mp9q7jzjl6wvq82gfl3jd7aicwrl9ra07b")))
    (package
      (name "publishing-api")
-     (version "release_1650")
+     (version "release_1651")
      (source
       (github-archive
        #:repository "publishing-api"
        #:commit-ish version
-       #:hash (base32 "1k5a0qwb29whgrhdvccy9ba2zw02pvrl49yqj3w67fkqkxxpyqcj")))
+       #:hash (base32 "0dmmnil3gkg3xzwxnf6hvapay3sxh1hjadpgzpfkv3rdmdmk5sqh")))
      (build-system rails-build-system)
      (arguments '(#:precompile-rails-assets? #f))
      (synopsis "Service for storing and providing workflow for GOV.UK content")
