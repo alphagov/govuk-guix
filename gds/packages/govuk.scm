31804346ec52c3995185b627ce10960f10b8dda0
Package Updater <govuk-guix-package-updater@digital.cabinet-office.gov.uk>
2020-05-05 17:30:08 +0100

    Update email-alert-frontend from release_571 to release_572

--- gds/packages/govuk.scm
+++ gds/packages/govuk.scm
@@ -457,15 +457,15 @@ proxies requests to some upstream")
 (define-public email-alert-frontend
   (package-with-bundler
    (bundle-package
-    (hash (base32 "0wzn224bbmb3xaw62g9lcpl987dxyd2hz9i6dw3ij1d4hzl7s78q")))
+    (hash (base32 "148h9l2if5vzy6n48mqzr37dg233cka0i5h815hp8fvihmm7iz92")))
    (package
      (name "email-alert-frontend")
-     (version "release_571")
+     (version "release_572")
      (source
       (github-archive
        #:repository name
        #:commit-ish version
-       #:hash (base32 "0k2wx6jlyw2fab5scadfbqipzpr2m2fswh9xz33nkx64gx2r6525")))
+       #:hash (base32 "0a3ksdz8sgfxvh3ijapb4zb1knr4i9ra7rnznlmzbi1w74cqx2zh")))
      (build-system rails-build-system)
      (synopsis "")
      (description "")
