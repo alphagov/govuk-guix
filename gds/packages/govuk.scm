7de81b5260dd8a73a23ad24215b31894a2b062db
Package Updater <govuk-guix-package-updater@digital.cabinet-office.gov.uk>
2020-05-14 15:23:55 +0100

    Update publisher from release_2298 to release_2299

--- gds/packages/govuk.scm
+++ gds/packages/govuk.scm
@@ -1154,12 +1154,12 @@ data to use can be selected, and multiple backends are supported.")
     (hash (base32 "18lmpjiv8axxhzg22x5aj4m1b8q3pc3xs134zii2g41n9i1jin8v")))
    (package
      (name "publisher")
-     (version "release_2298")
+     (version "release_2299")
      (source
       (github-archive
        #:repository name
        #:commit-ish version
-       #:hash (base32 "053fq0yqxnj8is1chhwg8x9kmsa9n4aql5qvma4wi8p7j2ap7zdw")))
+       #:hash (base32 "1pbhniwx6fqvyhf7c7p7vnv0mf3h2fmx6v3dc94kvdgc943psh0w")))
      (build-system rails-build-system)
      (arguments
       `(#:phases
