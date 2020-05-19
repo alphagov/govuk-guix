1bc84b037f3186c056c9dba7236aa8da3c24267f
Package Updater <govuk-guix-package-updater@digital.cabinet-office.gov.uk>
2020-05-19 09:47:59 +0100

    Update content-store from release_1011 to release_1012

--- gds/packages/govuk.scm
+++ gds/packages/govuk.scm
@@ -384,15 +384,15 @@ proxies requests to some upstream")
 (define-public content-store
   (package-with-bundler
    (bundle-package
-    (hash (base32 "188vypx847brn63mxp2bv5d4n0p8ci6m4dd6ysa7qyh88b4aivfa")))
+    (hash (base32 "1vgg8mlsqv7155z1bs99wmfbsrj7c0s4wn47padkpigs1j8y0h0g")))
    (package
      (name "content-store")
-     (version "release_1011")
+     (version "release_1012")
      (source
       (github-archive
        #:repository name
        #:commit-ish version
-       #:hash (base32 "1j5rwa5n15081d9ivvvzs37bf33abfdg9clj514hi5ca8cgg6qrv")))
+       #:hash (base32 "1qcq8drckgkrcz07ysxz1xcnqnva9rilwlhqjpmad8j3g3s94rj1")))
      (build-system rails-build-system)
      (arguments '(#:precompile-rails-assets? #f))
      (synopsis "")
