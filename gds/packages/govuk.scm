c9f75642e6f279aef7943b24220f809a275df403
Package Updater <govuk-guix-package-updater@digital.cabinet-office.gov.uk>
2020-05-22 20:23:01 +0100

    Update release from release_634 to release_635

--- gds/packages/govuk.scm
+++ gds/packages/govuk.scm
@@ -1255,12 +1255,12 @@ content, as well as broadcasting changes to a message queue.")
     (hash (base32 "1iji6gbrcggknaiqmw83471wg0f4xp1n9ablcka0i2lv86285v6h")))
    (package
      (name "release")
-     (version "release_634")
+     (version "release_635")
      (source
       (github-archive
        #:repository name
        #:commit-ish version
-       #:hash (base32 "0lvkalvzd9vh1q4nfhlkagfz4ymibzfkcpi880x93xk307zbjpkc")))
+       #:hash (base32 "1nlvpldkp9261jfp9i5dryf7lngl61ylqk1phy5kgswqd7yvvlj1")))
      (build-system rails-build-system)
      (arguments
       `(#:phases
