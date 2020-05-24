78655ec726773205107600fe38b4e8cabd7ca037
Christopher Baines <christopher.baines@digital.cabinet-office.gov.uk>
2020-05-24 12:50:29 +0100

    Remove mongodb-create-user-and-database
    As it's unused.

--- gds/services/utils/databases/mongodb.scm
+++ gds/services/utils/databases/mongodb.scm
@@ -28,42 +28,6 @@
         (default 27017))
   (database mongodb-connection-config-database))
 
-(define mongodb-create-user-and-database
-  (match-lambda
-   (($ <mongodb-connection-config> user password host port database)
-    #~(lambda ()
-        (let
-            ((pid (primitive-fork))
-             (mongodb-user (getpwnam "mongodb"))
-             (mongo (string-append #$mongodb "/bin/mongo")))
-          (if
-           (= 0 pid)
-           (dynamic-wind
-               (const #t)
-               (lambda ()
-                 (setgid (passwd:gid mongodb-user))
-                 (setuid (passwd:uid mongodb-user))
-                 (let ((p (open-pipe* OPEN_WRITE mongo "--port" (number->string #$port))))
-                   (display "\nChecking if user exists:\n")
-                   (simple-format p "
-use ~A
-db.createUser(
-  {
-    user: \"~A\",
-    pwd: \"~A\",
-    roles: [
-       { role: \"readWrite\", db: \"~A\" }
-    ]
-  }
-)
-" #$database #$user #$password #$database)
-                   (close-pipe p))
-                 (primitive-exit 0))
-               (lambda ()
-                 (primitive-exit 1)))
-           (waitpid pid)))))))
-
-
 (define mongodb-create-user-for-database-connection
   (match-lambda
     (($ <mongodb-connection-config> user password host port database)
