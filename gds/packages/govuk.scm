79f99b85e4ccfdce1b69c67629be2c14260976b6
Package Updater <govuk-guix-package-updater@digital.cabinet-office.gov.uk>
2020-06-11 09:22:25 +0100

    Update feedback from release_872 to release_873

--- gds/packages/govuk.scm
+++ gds/packages/govuk.scm
@@ -498,15 +498,15 @@ proxies requests to some upstream")
 (define-public feedback
   (package-with-bundler
    (bundle-package
-    (hash (base32 "1baki0hlxwg6f9c7by5584836fa9wram8qphvrbkzmlwbmsllssd")))
+    (hash (base32 "0iilfgxyrw3zax09bm730khrv1kpqrh332icr48hsyr90aw94bpj")))
    (package
      (name "feedback")
-     (version "release_872")
+     (version "release_873")
      (source
       (github-archive
        #:repository name
        #:commit-ish version
-       #:hash (base32 "15z1phsvm9r8y04d78an76dssmwf38sg6ycydx8qsdlsfmyidhy0")))
+       #:hash (base32 "03h7714bkaw0mqnxmcpxyldl59blp9pi70vq11ydwp95sbsp4ff8")))
      (build-system rails-build-system)
      (arguments
       '(#:phases
