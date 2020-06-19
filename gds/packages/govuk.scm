2f2eec9e40dcf8026f38f48b083e0d4ccbd41c42
Package Updater <govuk-guix-package-updater@digital.cabinet-office.gov.uk>
2020-06-19 11:28:58 +0100

    Update smart-answers from release_4819 to release_4820

--- gds/packages/govuk.scm
+++ gds/packages/govuk.scm
@@ -1545,12 +1545,12 @@ production:
     (hash (base32 "1vz9qkqvc9pr5l4dksdya8pr3x7bm7dpq75yvc5n3m7f4947f73c")))
    (package
      (name "smart-answers")
-     (version "release_4819")
+     (version "release_4820")
      (source
       (github-archive
        #:repository name
        #:commit-ish version
-       #:hash (base32 "0azqfqai8kzjamw48azc0hk3vqk7b1nxzsprjrsl6q4m57xs98zb")))
+       #:hash (base32 "1nld637i7p76fc5wdn07lzm2dzb745n2wm7zbm9paq35qi9ww1d1")))
      (build-system rails-build-system)
      ;; Asset precompilation fails due to the preload_working_days
      ;; initialiser
