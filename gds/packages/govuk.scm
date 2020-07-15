f38e0d7f27a227cb1f666abcd7241892d7f1f10a
Package Updater <govuk-guix-package-updater@digital.cabinet-office.gov.uk>
2020-07-15 09:19:44 +0100

    Update imminence from release_652 to release_653

--- gds/packages/govuk.scm
+++ gds/packages/govuk.scm
@@ -794,12 +794,12 @@ service setup.")
     (hash (base32 "1bc4q9ry0pws0z9a4dkl56rbnsfb8sv1pfj4mrbjm65cg63l285a")))
    (package
      (name "imminence")
-     (version "release_652")
+     (version "release_653")
      (source
       (github-archive
        #:repository name
        #:commit-ish version
-       #:hash (base32 "07rbj7anqvbk9bbbxz52bay4zayj18xyhlgl3phy8qybcxkcr1rw")))
+       #:hash (base32 "0j80am9xr55i6fr1xhv9yw0490gsxdf6k0hkyzwk6cbsglplcqyw")))
      (build-system rails-build-system)
      (arguments
       `(#:phases
