5cebb9149f1034d8cf6c29f0fd5332db450d6bc0
Package Updater <govuk-guix-package-updater@digital.cabinet-office.gov.uk>
2020-08-24 10:47:34 +0100

    Update maslow from release_564 to release_567

--- gds/packages/govuk.scm
+++ gds/packages/govuk.scm
@@ -968,15 +968,15 @@ service setup.")
 (define-public maslow
   (package-with-bundler
    (bundle-package
-    (hash (base32 "1alp701ppachdwv1q5nys9p84zyjmjnwkvjkhlwx97hpdnwpn7hf")))
+    (hash (base32 "034lh1fkygk64jvq0zbgrqki5dvv07cdf00hd3sdj5m5k3lfgzw8")))
    (package
      (name "maslow")
-     (version "release_564")
+     (version "release_567")
      (source
       (github-archive
        #:repository name
        #:commit-ish version
-       #:hash (base32 "09gvgk8xd2k38wcdlgxccqibnxi2dlyy74mpw8g3lbwy2avyd66z")))
+       #:hash (base32 "076bnyzxhial8d3v9dswj02lln6nr9cbrl6ax3avi9icf4pfka7a")))
      (build-system rails-build-system)
      (arguments
       `(#:phases
